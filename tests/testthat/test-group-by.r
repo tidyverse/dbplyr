2c3597071edcb1b6204514e0fe99638c135b7f86
hadley <h.wickham@gmail.com>
2017-03-28 10:52:19 -0500

    Simplify grouping tests

--- tests/testthat/test-group-by.r
+++ tests/testthat/test-group-by.r
@@ -1,43 +1,33 @@
 context("group_by")
 
-df <- data.frame(x = rep(1:3, each = 10), y = rep(1:6, each = 5))
-
-tbls <- test_load(df)
-
 test_that("group_by with add = TRUE adds groups", {
-  add_groups1 <- function(tbl) group_by(tbl, x, y, add = TRUE)
-  add_groups2 <- function(tbl) group_by(group_by(tbl, x, add = TRUE), y, add = TRUE)
+  mf <- memdb_frame(x = 1:3, y = 1:3)
+  gf1 <- mf %>% group_by(x, y)
+  gf2 <- mf %>% group_by(x) %>% group_by(y, add = TRUE)
 
-  expect_groups(add_groups1(tbls$df), c("x", "y"))
-  expect_groups(add_groups2(tbls$df), c("x", "y"))
-
-  skip_if_no_sqlite()
-  expect_groups(add_groups1(tbls$sqlite), c("x", "y"))
-  expect_groups(add_groups2(tbls$sqlite), c("x", "y"))
+  expect_equal(group_vars(gf1), c("x", "y"))
+  expect_equal(group_vars(gf2), c("x", "y"))
 })
 
 test_that("collect, collapse and compute preserve grouping", {
-  skip_if_no_sqlite()
   g <- memdb_frame(x = 1:3, y = 1:3) %>% group_by(x, y)
 
-  expect_groups(compute(g), c("x", "y"))
-  expect_groups(collapse(g), c("x", "y"))
-  expect_groups(collect(g), c("x", "y"))
+  expect_equal(group_vars(compute(g)), c("x", "y"))
+  expect_equal(group_vars(collapse(g)), c("x", "y"))
+  expect_equal(group_vars(collect(g)), c("x", "y"))
 })
 
 test_that("joins preserve grouping", {
-  for (tbl in tbls) {
-    g <- group_by(tbl, x)
+  g <- memdb_frame(x = 1:3, y = 1:3) %>% group_by(x)
 
-    expect_groups(inner_join(g, g, by = c("x", "y")), "x")
-    expect_groups(left_join (g, g, by = c("x", "y")), "x")
-    expect_groups(semi_join (g, g, by = c("x", "y")), "x")
-    expect_groups(anti_join (g, g, by = c("x", "y")), "x")
-  }
+  expect_equal(group_vars(inner_join(g, g, by = c("x", "y"))), "x")
+  expect_equal(group_vars(left_join(g, g, by = c("x", "y"))), "x")
+  expect_equal(group_vars(semi_join(g, g, by = c("x", "y"))), "x")
+  expect_equal(group_vars(anti_join(g, g, by = c("x", "y"))), "x")
 })
 
-test_that("group_by can perform mutate (on database)", {
-  mf <- memdb_frame(x = c(3:1), y = c(1:3))
+test_that("group_by can perform mutate", {
+  mf <- memdb_frame(x = 3:1, y = 1:3)
   out <- mf %>%
     group_by(z = x + y) %>%
     summarise(n = n()) %>%
--- tests/testthat/test-summarise.r
+++ tests/testthat/test-summarise.r
@@ -1,13 +1,10 @@
 context("Summarise")
 
-df <- data.frame(x = rep(1:4, each = 4), y = rep(1:2, each = 8), z = runif(16))
-tbls <- test_load(df)
-
 test_that("summarise peels off a single layer of grouping", {
-  for (i in seq_along(tbls)) {
-    grouped <- group_by(tbls[[i]], x, y)
-    summed <- summarise(grouped, n())
+  mf1 <- memdb_frame(x = 1, y = 1, z = 2) %>% group_by(x, y)
+  mf2 <- mf1 %>% summarise(n = n())
+  expect_equal(group_vars(mf2), "x")
 
-    expect_groups(summed, "x", info = names(tbls)[i])
-  }
+  mf3 <- mf2 %>% summarise(n = n())
+  expect_null(group_vars(mf3))
 })
