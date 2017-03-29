aeeddc8d563f1b73a1e2fafc425f6f2621bcd613
hadley <h.wickham@gmail.com>
2017-03-29 08:37:33 -0500

    Leave explain generic in dplyr

--- R/explain.r
+++ R/explain.r
@@ -1,39 +1,20 @@
-#' Explain details of a tbl
+#' Show query
 #'
-#' This is a generic function which gives more details about an object than
-#' [print()], and is more focussed on human readable output than
-#' [str()].
-#'
-#' @section Databases:
-#' Explaining a `tbl_sql` will run the SQL `EXPLAIN` command which
-#' will describe the query plan. This requires a little bit of knowledge about
-#' how `EXPLAIN` works for your database, but is very useful for
-#' diagnosing performance problems.
-#'
+#' @param x A [tbl_dbi]
 #' @export
-#' @param x An object to explain
-#' @param ... Other parameters possibly used by generic
-#' @return The first argument, invisibly.
 #' @examples
 #' library(dplyr)
 #'
 #' lahman_s <- lahman_sqlite()
 #' batting <- tbl(lahman_s, "Batting")
 #' batting %>% show_query()
-#' batting %>% explain()
-#'
-#' # The batting database has indices on all ID variables:
-#' # SQLite automatically picks the most restrictive index
-#' batting %>% filter(lgID == "NL" & yearID == 2000L) %>% explain()
-#'
-#' # OR's will use multiple indexes
-#' batting %>% filter(lgID == "NL" | yearID == 2000) %>% explain()
-#'
-#' # Joins will use indexes in both tables
-#' teams <- tbl(lahman_s, "Teams")
-#' batting %>% left_join(teams, c("yearID", "teamID")) %>% explain()
-explain <- function(x, ...) {
-  UseMethod("explain")
+show_query <- function(x) {
+  con <- con_acquire(x$src)
+  on.exit(con_release(x$src, con), add = TRUE)
+
+  message("<SQL>\n", sql_render(x, con = con))
+
+  invisible(x)
 }
 
 #' @export
@@ -46,17 +27,6 @@ explain.tbl_sql <- function(x, ...) {
 
   message("\n")
   message("<PLAN>\n", db_explain(con, sql_render(x, con = con)))
-
-  invisible(x)
-}
-
-#' @export
-#' @rdname explain
-show_query <- function(x) {
-  con <- con_acquire(x$src)
-  on.exit(con_release(x$src, con), add = TRUE)
-
-  message("<SQL>\n", sql_render(x, con = con))
 
   invisible(x)
 }
