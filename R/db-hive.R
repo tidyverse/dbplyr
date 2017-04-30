f225b9e55c77612702632533dca077f34bf2aa97
edgararuiz <edgararuiz@gmail.com>
2017-04-29 20:48:09 -0500

    Adding ifelse functions to Hive

--- R/db-hive.R
+++ R/db-hive.R
@@ -25,8 +25,10 @@ sql_translate_env.Hive <- function(con) {
       as.logical = function(x) build_sql("CAST(", x, " AS BOOLEAN)"),
       as.character  = function(x) build_sql("CAST(", x, " AS STRING)"),
       as.Date  = function(x) build_sql("CAST(", x, " AS DATE)"),
-      round = function(x, digits = 0L) build_sql("ROUND(", x, ",", digits,")"),
-      paste0 = function(...) build_sql("CONCAT", list(...))
+      round = function(x, digits = 0L) build_sql("ROUND(", x, ", INT(", digits,"))"),
+      paste0 = function(...) build_sql("CONCAT", list(...)),
+      if_else = function(condition, true, false) build_sql("if(", condition, ", ", true, ", ", false, ")"),
+      if_else = function(test, yes, no) build_sql("if(", test, ", ", yes, ", ", no, ")")
     ) ,
     sql_translator(
       .parent = base_agg,
