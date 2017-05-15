68a0dd0ff40ff4425d9d9fb8b8c068b5ba7b0f88
hadley <h.wickham@gmail.com>
2017-05-15 09:36:57 -0500

    Remove extraneous context

--- tests/testthat/test-translate-MySQL.R
+++ tests/testthat/test-translate-MySQL.R
@@ -1,8 +1,6 @@
 context("translate-MySQL")
 
 test_that("use CHAR type for as.character", {
-  context("translate-MSSQL")
-
   expect_equivalent(
     translate_sql(as.character(x), con = simulate_mysql()),
     sql("CAST(`x` AS CHAR)")
