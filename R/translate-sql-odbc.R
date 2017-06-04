e43e2fe0e19bee6cb3f4d675b819a02f97cd1784
edgararuiz <edgararuiz@gmail.com>
2017-06-04 13:38:03 -0400

    Testing change to window functions

--- R/translate-sql-odbc.R
+++ R/translate-sql-odbc.R
@@ -26,7 +26,14 @@ base_odbc_scalar <- sql_translator(
 base_odbc_agg <- sql_translator(
   .parent = base_agg,
   n             = function() sql("COUNT(*)"),
-  count         = function() sql("COUNT(*)")
+  count         = function() sql("COUNT(*)"),
+  first         = function(x, order = NULL) {
+    win_over(
+      build_sql("first_value", list(x)),
+      win_current_group(),
+      order %||% win_current_order() %||% win_current_group() %||% list(x)
+    )
+  }
 )
 
 #' @export
@@ -35,7 +42,14 @@ base_odbc_agg <- sql_translator(
 base_odbc_win <- sql_translator(
   .parent = base_win,
   n             = function() sql("COUNT(*)"),
-  count         = function() sql("COUNT(*)")
+  count         = function() sql("COUNT(*)"),
+  first         = function(x, order = NULL) {
+    win_over(
+      build_sql("first_value", list(x)),
+      win_current_group(),
+      order %||% win_current_order() %||% win_current_group() %||% list(x)
+    )
+    }
 )
 
 #' @export
