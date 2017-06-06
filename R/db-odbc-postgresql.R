8cb83921f36b5d8cbef19a4397ce02a909e09921
edgararuiz <edgararuiz@gmail.com>
2017-06-05 17:19:07 -0700

    Removing db_write_table from postgres

--- R/db-odbc-postgresql.R
+++ R/db-odbc-postgresql.R
@@ -23,13 +23,6 @@ db_explain.PostgreSQL <- function(con, sql, format = "text", ...) {
   db_explain.PostgreSQLConnection(con, sql, format = format, ...)
 }
 
-#' @export
-db_write_table.PostgreSQL <- function(con, table, types, values,
-                                      temporary = TRUE, ...) {
-
-  db_write_table.PostgreSQLConnection(con, table, types, values,
-                                      temporary = temporary, ...)
-}
 
 #' @export
 db_query_fields.PostgreSQL <- function(con, sql, ...) {
