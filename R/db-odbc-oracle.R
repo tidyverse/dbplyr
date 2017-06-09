4c3678c365b054513859a79cb2456cd5ac18c5ed
edgararuiz <edgararuiz@gmail.com>
2017-06-09 11:22:27 -0700

    Adding db_copy_to for Oracle

--- R/db-odbc-oracle.R
+++ R/db-odbc-oracle.R
@@ -107,4 +107,95 @@ db_analyze.Oracle <- function(con, table, ...) {
   DBI::dbExecute(con, sql)
 }
 
+#' @export
+db_copy_to.Oracle <- function(con, table, values,
+                              overwrite = FALSE, types = NULL, temporary = FALSE,
+                              unique_indexes = NULL, indexes = NULL,
+                              analyze = TRUE, ...) {
 
+
+  # Oracle requires case matching for the table in INSERT
+  # operation, easier to upper case name in the beginning
+
+  table <- toupper(table)
+
+  found <- db_has_table(con, table)
+  if (found && !overwrite && !append) {
+    stop("Table ", table, " exists in database, and both overwrite and",
+         " append are FALSE", call. = FALSE)
+  }
+
+  tryCatch({
+
+    # Using transactions to both create and insert into the table, like in a
+    # a Store Procedure. This enables us to execute multiple smaller INSERT INTO
+    # commands instead of one large string with all of the values that may
+    # overflow a string buffer and to prevent leaving an empty table behind
+    # if the INSERT INTO fails. As of 5/28/17, the full ROLLBACK does not work
+    # we may need support for END and SAVEPOINT (
+    # https://stackoverflow.com/questions/11966020/begin-end-block-atomic-transactions-in-pl-sql
+    # )
+
+    dbBegin(con)
+
+    if (found && overwrite) {
+      db_drop_table(con, table)
+    }
+
+    values <- sqlData(con, row.names = TRUE, values[, , drop = FALSE])
+
+    if (!found || overwrite) {
+      # Oracle does not like quote marks for table and field names
+      # conn@quote will be set to a single quote in the section
+      # where we insert the records
+      con@quote <- ""
+      sql <- sqlCreateTable(con, table, values, row.names = FALSE, temporary = FALSE)
+      dbExecute(con, sql)
+    }
+
+
+    if (nrow(values) > 0) {
+
+      con@quote <- "'"
+      fields <- paste0(colnames(values), ", ", collapse = "")
+      fields <- substr(fields, 1, nchar(fields) - 2)
+
+      lapply(1:nrow(values),
+             function(x){
+               row_values <- paste0(
+                 dbQuoteIdentifier(con, as.character(values[x, ])),
+                 ", ",
+                 collapse = ""
+               )
+               row_values <- substr(row_values, 1, nchar(row_values) - 2)
+               insert_sql <- paste0("INSERT INTO ", toupper(table), " (", fields, ") VALUES (", row_values, ")")
+               dbExecute(con, insert_sql)
+             }
+      )
+
+    }
+
+
+    dbCommit(con)
+  }  ,  error = function(err) {
+    dbRollback(con)
+    stop(err)
+  })
+
+  table
+}
+
+#' @export
+db_list_tables.Oracle <- function(con)  {
+  table_names <- dbGetQuery(con, "select table_name from dba_tables")
+  table_names$upper <- toupper(table_names$TABLE_NAME)
+  table_names <- table_names[table_names$TABLE_NAME == table_names$upper , 1]
+  as.character(table_names)
+}
+
+#' @export
+db_has_table.Oracle <- function(con, table){
+  stopifnot(length(table) == 1)
+  table <- toupper(table)
+  dbUnQuoteIdentifier(con, table) %in% dbListTables(con)
+}
