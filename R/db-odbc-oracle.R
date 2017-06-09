a164717c35a9695f855075580f7c7ea741705b71
edgararuiz <edgararuiz@gmail.com>
2017-06-09 14:08:15 -0700

    Removing setmethod sqlcreatetable

--- R/db-odbc-oracle.R
+++ R/db-odbc-oracle.R
@@ -196,30 +196,3 @@ db_has_table.Oracle <- function(con, table){
   stopifnot(length(table) == 1)
   toupper(table) %in% db_list_tables(con)
 }
-
-# Temporary sqlCreateTable for Oracle until odbc is patched
-setMethod("sqlCreateTable", "Oracle",
-          function(con, table, fields, row.names = NA, temporary = FALSE, ...) {
-            table <- dbQuoteIdentifier(con, table)
-
-            if (is.data.frame(fields)) {
-              fields <- sqlRownamesToColumn(fields, row.names)
-              fields <- vapply(fields, function(x) DBI::dbDataType(con, x), character(1))
-            }
-
-            field_names <- dbQuoteIdentifier(con, names(fields))
-            field_types <- unname(fields)
-            fields <- paste0(field_names, " ", field_types)
-
-            SQL(paste0(
-              "CREATE ", if (temporary) " GLOBAL TEMPORARY ", "TABLE ", table, " (\n",
-              "  ", paste(fields, collapse = ",\n  "), "\n)\n", if (temporary) " ON COMMIT DELETE ROWS"
-            ))
-          }
-)
-
-
-#' @export
-# Temporary setClass() for Oracle until odbc is patched
-setClass("Oracle", contains = c("DBIConnection", "VIRTUAL"))
-
