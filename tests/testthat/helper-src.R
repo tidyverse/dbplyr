271ad8da8ccdedac6956821c4788e211f87b0572
hadley <h.wickham@gmail.com>
2017-10-27 08:58:29 -0500

    Don't use MySQL test dbs on travis

--- tests/testthat/helper-src.R
+++ tests/testthat/helper-src.R
@@ -2,16 +2,6 @@ if (test_srcs$length() == 0) {
 
   test_register_src("df", dplyr::src_df(env = new.env(parent = emptyenv())))
   test_register_con("sqlite", RSQLite::SQLite(), ":memory:")
-  test_register_con("mysql", RMySQL::MySQL(),
-    dbname = "test",
-    host = "localhost",
-    user = Sys.getenv("USER")
-  )
-  test_register_con("MariaDB", RMariaDB::MariaDB(),
-    dbname = "test",
-    host = "localhost",
-    user = Sys.getenv("USER")
-  )
 
   if (identical(Sys.getenv("TRAVIS"), "true")) {
     test_register_con("postgres", RPostgreSQL::PostgreSQL(),
@@ -20,6 +10,16 @@ if (test_srcs$length() == 0) {
       password = ""
     )
   } else {
+    test_register_con("mysql", RMySQL::MySQL(),
+      dbname = "test",
+      host = "localhost",
+      user = Sys.getenv("USER")
+    )
+    test_register_con("MariaDB", RMariaDB::MariaDB(),
+      dbname = "test",
+      host = "localhost",
+      user = Sys.getenv("USER")
+    )
     test_register_con("postgres", RPostgreSQL::PostgreSQL(),
       dbname = "test",
       host = "localhost",
