74fb43b4e86ce852ffb27bf3a937ea11de78d742
Kirill Müller <krlmlr+r@mailbox.org>
2017-11-03 11:08:35 +0100

    don't rely on output files

--- tests/testthat/test-output.R
+++ tests/testthat/test-output.R
@@ -7,25 +7,44 @@ test_that("ungrouped output", {
   mtcars_mem <- src_memdb() %>%
     copy_to(mtcars, name = "mtcars-output-test", overwrite = TRUE)
   iris_mem <- src_memdb() %>%
-    copy_to(iris, name = "iris-output-test", overwrite = TRUE) %>%
-    group_by(Species) %>%
-    arrange(Sepal.Length)
+    copy_to(iris, name = "iris-output-test", overwrite = TRUE)
 
   withr::with_options(
     list(digits = 4, width = 80),
     with_mock(
       `dbplyr::sqlite_version` = function() "x.y.z",
-      {
-        expect_output_file_rel(
-          cat(tbl_sum(mtcars_mem), sep = "\n"),
-          "mtcars.txt"
+      expect_equal(
+        mtcars_mem %>%
+          tbl_sum(),
+        c(
+          Source = "table<mtcars-output-test> [?? x 11]",
+          Database = "sqlite x.y.z [:memory:]"
         )
+      ),
 
-        expect_output_file_rel(
-          cat(tbl_sum(iris_mem), sep = "\n"),
-          "iris.txt"
+      expect_equal(
+        mtcars_mem %>%
+          group_by(cyl, gear) %>%
+          tbl_sum(),
+        c(
+          Source = "table<mtcars-output-test> [?? x 11]",
+          Database = "sqlite x.y.z [:memory:]",
+          Groups = "cyl, gear"
         )
-      }
+      ),
+
+      expect_equal(
+        iris_mem %>%
+          group_by(Species) %>%
+          arrange(Sepal.Length, Sepal.Width) %>%
+          tbl_sum(),
+        c(
+          Source = "table<iris-output-test> [?? x 5]",
+          Database = "sqlite x.y.z [:memory:]",
+          Groups = "Species",
+          "Ordered by" = "Sepal.Length, Sepal.Width"
+        )
+      )
     )
   )
 })
