fdc9792f9723d7c192b742dc13dd3c199e92f08b
Cole Arendt <cole@rstudio.com>
2018-03-15 20:37:57 +0000

    reuse more elegant implementation from substr

--- R/translate-sql-base.r
+++ R/translate-sql-base.r
@@ -180,21 +180,10 @@ base_scalar <- sql_translator(
                         ,"))"
                       )},
   str_sub = function(string, start = 1L, end = -1L) {
-    build_sql(
-      "SUBSTR("
-      , string
-      , ","
-      , as.integer(start)
-      , sql(ifelse(end > -1L
-               , ifelse(
-                 end < start
-                 ,",0"
-                 , paste0(",",as.integer(end-start+1))
-                 )
-               , "")
-      )
-      , ")"
-    )
+    start <- as.integer(start)
+    length <- pmax(as.integer(stop) - start + 1L, 0L)
+
+    build_sql(sql("substr"), list(x, start, length))
   }
 )
 
