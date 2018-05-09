47ffe1844c3b53c2489583da92e96f2004aaf51e
kevinykuo <kevinykuo@gmail.com>
2018-05-08 22:11:21 -0400

    ungroup before selecting in pull.tbl_sql()

--- R/tbl-sql.r
+++ R/tbl-sql.r
@@ -75,6 +75,7 @@ pull.tbl_sql <- function(.data, var = -1) {
   expr <- enquo(var)
   var <- dplyr:::find_var(expr, tbl_vars(.data))
 
+  .data <- ungroup(.data)
   .data <- select(.data, !! sym(var))
   .data <- collect(.data)
   .data[[1]]
