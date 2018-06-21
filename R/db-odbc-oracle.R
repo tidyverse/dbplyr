c1e8705ccc6f36e87fc522925d370bf9455add2e
edgararuiz <edgar@rstudio.com>
2018-06-21 12:55:45 -0500

    corrected function call

--- R/db-odbc-oracle.R
+++ R/db-odbc-oracle.R
@@ -40,7 +40,7 @@ sql_translate_env.Oracle <- function(con) {
       # bit64::as.integer64 can translate to BIGINT for some
       # vendors, which is equivalent to NUMBER(19) in Oracle
       # https://docs.oracle.com/cd/B19306_01/gateways.102/b14270/apa.htm
-      as.numeric64  = sql_cast("NUMBER(19)"),
+      as.integer64  = sql_cast("NUMBER(19)"),
       as.numeric    = sql_cast("NUMBER"),
       as.double     = sql_cast("NUMBER")
     ),
