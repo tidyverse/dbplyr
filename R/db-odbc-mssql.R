5f11928599246b2803b401a3e7cd41f300c56211
Cole Arendt <cole@rstudio.com>
2018-10-10 20:11:27 +0000

    added related so post

--- R/db-odbc-mssql.R
+++ R/db-odbc-mssql.R
@@ -95,6 +95,8 @@
                               "CHARINDEX(", pattern, ", ", string, ") > 0"
                             )},
       # mssql does not support INITCAP, toTitle, toPropCase, etc.
+      # related SO post:
+      # https://stackoverflow.com/questions/230138/sql-server-make-all-upper-case-to-proper-case-title-case
       str_to_title    = sql_not_supported("str_to_title()")
     ),
     sql_translator(.parent = base_odbc_agg,
