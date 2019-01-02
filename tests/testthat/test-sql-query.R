acf6a332ea51ce18d8f281294f0d3bb4e72e5e9b
Hadley Wickham <h.wickham@gmail.com>
2019-01-02 14:19:10 -0600

    Convert select_query() test to use expect_known_output()

--- tests/testthat/test-sql-query.R
+++ tests/testthat/test-sql-query.R
@@ -7,11 +7,5 @@ test_that("add_suffixes works if no suffix requested", {
 
 test_that("select_query() print method output is as expected", {
   mf <- select_query(lazy_frame(x = 1, src = simulate_dbi()))
-  expect_equal(
-    paste0(
-      capture.output(print.select_query(mf)),
-      collapse = " "
-    ),
-    "<SQL SELECT> From:     df Select:   *"
-  )
+  expect_known_output(mf, test_path("test-sql-query-select.txt"), print = TRUE)
 })
