883297236cdc7ffc8aba0098f186f1e1369837ab
Hadley Wickham <h.wickham@gmail.com>
2019-01-10 17:38:30 -0600

    Clarify escaping test

--- tests/testthat/test-escape.R
+++ tests/testthat/test-escape.R
@@ -1,21 +1,19 @@
-context("escape")
+context("test-escape.R")
 
-# See test-sql-escape for SQL generation tests. These tests that the
-# generate SQL actually works across the three main open source backends
-
-test_that("multiplication works", {
-  dfs <- test_frame(x = c(1, NA)) %>%
-    lapply(. %>% mutate(y = coalesce(x > 0, TRUE)))
+# Test full logical escaping round trip on databases
+test_that("logical values correctly escaped", {
+  src <- test_frame(x = c(TRUE, FALSE))
+  dfs <- src %>% lapply(. %>% mutate(y = x == TRUE))
   out <- dfs %>% lapply(collect)
 
   # SQLite treats as integers
-  expect_identical(out$sqlite$y, c(1L, 1L))
+  expect_identical(out$sqlite$y, c(1L, 0L))
 
   # MySQL converts to doubles
   if (!is.null(out$mysql))
-    expect_identical(out$mysql$y, c(1, 1))
+    expect_identical(out$mysql$y, c(1, 0))
 
   # PostgresSQL keeps as logical
   if (!is.null(out$postgres))
-    expect_identical(out$postgres$y, c(TRUE, TRUE))
+    expect_identical(out$postgres$y, c(TRUE, FALSE))
 })
