6ccf7d8a091d96f362f9e46a7ea62ab11ebe5b8b
Hadley Wickham <h.wickham@gmail.com>
2019-01-11 10:24:59 -0600

    Remove explain test
    Because output differs across SQLite versions

--- tests/testthat/test-explain.R
+++
@@ -1,6 +0,0 @@
-context("test-explain.R")
-
-test_that("basic pipeline is correct", {
-  mf <- memdb_frame(x = 1:5, .name = "XYZ") %>% filter(x > 5)
-  expect_output_file(explain(mf), "test-explain-sqlite.txt")
-})
