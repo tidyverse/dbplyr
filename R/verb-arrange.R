ae523329041d1e415d6f8c31f86c58f9a60baf92
Michael W. Kearney <kearneymw@missouri.edu>
2019-01-19 11:53:39 -0600

    document arrange.tbl_lazy

--- R/verb-arrange.R
+++ R/verb-arrange.R
@@ -1,3 +1,28 @@
+#' Arrange rows by variables in a local lazy tibble
+#'
+#' Order tbl_lazy rows by an expression involving its variables.
+#'
+#' @section Missing values:
+#' When applied to `tbl_lazy`, the [arrange()] method sorts NAs first (at the
+#' beginning) compared to the default sort behavior on data frames, which places
+#' NA values last (at the end).
+#'
+#' @inheritParams dplyr::arrange
+#' @return An object of the same class as `.data`.
+#' @examples
+#' library(dplyr)
+#'
+#' dbplyr::memdb_frame(a = c(3, 4, 1, 2)) %>%
+#'   arrange(a)
+#'
+#' # tbl_lazy: NAs sorted first
+#' dbplyr::memdb_frame(a = c(3, 4, NA, 2)) %>%
+#'   arrange(a)
+#'
+#' # tbl_df: NAs sorted last
+#' tibble(a = c(3, 5, NA, 2)) %>%
+#'   arrange(a)
+#'
 #' @export
 arrange.tbl_lazy <- function(.data, ...) {
   dots <- quos(...)
