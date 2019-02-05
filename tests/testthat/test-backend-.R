d5014b1c03188bc9f7a3d7771b07c784500bd69d
Hadley Wickham <h.wickham@gmail.com>
2019-02-05 09:14:58 -0600

    Move test out of test-dbi-s3.R

--- tests/testthat/test-backend-.R
+++ tests/testthat/test-backend-.R
@@ -1,5 +1,24 @@
 context("translate-math")
 
+test_that("db_write_table calls dbQuoteIdentifier on table name" ,{
+  idents <- character()
+
+  setClass("DummyDBIConnection", representation("DBIConnection"))
+  setMethod("dbQuoteIdentifier", c("DummyDBIConnection", "character"),
+    function(conn, x, ...) {
+      idents <<- c(idents, x)
+    }
+  )
+
+  setMethod("dbWriteTable", c("DummyDBIConnection", "character", "ANY"),
+    function(conn, name, value, ...) {TRUE}
+  )
+
+  dummy_con <- new("DummyDBIConnection")
+  db_write_table(dummy_con, "somecrazytablename", NA, NA)
+  expect_true("somecrazytablename" %in% idents)
+})
+
 
 # basic arithmetic --------------------------------------------------------
 
