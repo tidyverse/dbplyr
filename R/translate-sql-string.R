0ee3dc5d07bed9c0426270d61e13843f46ecdf4b
Cole Arendt <cole.arendt@outlook.com>
2019-02-07 21:26:33 -0500

    add notes on substr parameterization

--- R/translate-sql-string.R
+++ R/translate-sql-string.R
@@ -1,3 +1,6 @@
+# R prefers to specify start / stop or start / end
+# databases usually specify start / length
+# https://www.postgresql.org/docs/current/functions-string.html
 sql_substr <- function(x, start, stop) {
   start <- as.integer(start)
   length <- pmax(as.integer(stop) - start + 1L, 0L)
