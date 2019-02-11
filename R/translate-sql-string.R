b1e6a13cc2312b8a45b9030652d16e0c49482e16
Cole Arendt <cole.arendt@outlook.com>
2019-02-11 09:47:16 -0500

    update string helpers

--- R/translate-sql-string.R
+++ R/translate-sql-string.R
@@ -1,9 +1,26 @@
 # R prefers to specify start / stop or start / end
 # databases usually specify start / length
 # https://www.postgresql.org/docs/current/functions-string.html
-sql_substr <- function(x, start, stop) {
-  start <- as.integer(start)
-  length <- pmax(as.integer(stop) - start + 1L, 0L)
+#' @export
+#' @rdname sql_variant
+sql_substr <- function(f = "SUBSTR") {
+  function(x, start, end) {
+    start <- as.integer(start)
+    length <- pmax(as.integer(stop) - start + 1L, 0L)
 
-  sql_expr(SUBSTR(!!x, !!start, !!length))
+    sql_call2(f, x, start, length)
+  }
+}
+#' @export
+#' @rdname sql_variant
+sql_str_sub <- function(f = "SUBSTR") {
+  function(string, start = 1L, end = -1L) {
+    start <- as.integer(start)
+    if (end == -1L) {
+      return(sql_call2(f, string, start))
+    } else {
+      length <- pmax(as.integer(stop) - start + 1L, 0L)
+      return(sql_call2(f, string, start, length))
+    }
+  }
 }
