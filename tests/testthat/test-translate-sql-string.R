77bd57278b90a8b80ee8e7d7702259a8a8c46e20
Cole Arendt <cole.arendt@outlook.com>
2019-02-14 14:31:55 -0500

    add tests for str_sub behavior

--- tests/testthat/test-translate-sql-string.R
+++ tests/testthat/test-translate-sql-string.R
@@ -4,6 +4,7 @@ test_that("sql_substr works as expected", {
   setClass("DummyDBIConnection", representation("DBIConnection"))
 
   dummy_con <- new("DummyDBIConnection")
+  sql_context <- dbplyr:::sql_context
   prev_con <- sql_context$con
   sql_context$con <- dummy_con
   expect_error(sql_substr("SUBSTR")("test"), 'argument "start" is missing')
@@ -20,10 +21,26 @@ test_that("sql_str_sub works as expected", {
   setClass("DummyDBIConnection", representation("DBIConnection"))
 
   dummy_con <- new("DummyDBIConnection")
+  sql_context <- dbplyr:::sql_context
   prev_con <- sql_context$con
   sql_context$con <- dummy_con
 
-  sql_str_sub()("string")
+  expect_equal(sql_str_sub("SUBSTR")("string"), sql("SUBSTR('string', 1)"))
+  expect_equal(sql_str_sub("SUBSTRING")("string"), sql("SUBSTRING('string', 1)"))
+  expect_equal(sql_str_sub("SUBSTRING")("string", 4, 3), sql("SUBSTRING('string', 4, 0)"))
+  expect_equal(sql_str_sub("SUBSTRING")("string", 4, 5), sql("SUBSTRING('string', 4, 2)"))
+
+  # test that the "infinite length" default is respected
+  expect_equal(sql_str_sub("SUBSTRING")("string", 4), sql("SUBSTRING('string', 4)"))
+  expect_equal(
+    sql_str_sub("SUBSTRING", full_length = "compute")("string", 4),
+    sql("SUBSTRING('string', 4, LENGTH('string') - 4 + 1)")
+    )
+  expect_equal(
+    sql_str_sub("SUBSTRING", full_length = "compute", compute_method = "LEN")("string", 4),
+    sql("SUBSTRING('string', 4, LEN('string') - 4 + 1)")
+    )
 
   sql_context$con <- prev_con
 })
+
