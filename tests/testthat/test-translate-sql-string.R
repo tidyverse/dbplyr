011022443ee5782ae0304ec1201be01d1eb13b90
Cole Arendt <cole.arendt@outlook.com>
2019-02-14 21:41:35 -0500

    clean up test implementation work around strange stateful testing bug

--- tests/testthat/test-translate-sql-string.R
+++ tests/testthat/test-translate-sql-string.R
@@ -1,12 +1,11 @@
 context("translate string helpers")
 
 test_that("sql_substr works as expected", {
-  setClass("DummyDBIConnection", representation("DBIConnection"))
+  setClass("DummyDBIConnectionSQLSubstr", representation("DBIConnection"))
 
-  dummy_con <- new("DummyDBIConnection")
-  sql_context <- dbplyr:::sql_context
-  prev_con <- sql_context$con
-  sql_context$con <- dummy_con
+  dummy_con <- new("DummyDBIConnectionSQLSubstr")
+  prev_con <- dbplyr:::set_current_con(dummy_con)
+
   expect_error(sql_substr("SUBSTR")("test"), 'argument "start" is missing')
   expect_error(sql_substr("SUBSTR")("test",0), 'argument "stop" is missing')
   expect_equal(sql_substr("SUBSTR")("test",0,1), sql("SUBSTR('test', 0, 2)"))
@@ -14,16 +13,14 @@ test_that("sql_substr works as expected", {
   expect_equal(sql_substr("SUBSTR")("test",3,3), sql("SUBSTR('test', 3, 1)"))
   expect_equal(sql_substr("SUBSTRING")("string", 4, 5), sql("SUBSTRING('string', 4, 2)"))
 
-  sql_context$con <- prev_con
+  dbplyr:::set_current_con(prev_con)
 })
 
 test_that("sql_str_sub works as expected", {
-  setClass("DummyDBIConnection", representation("DBIConnection"))
+  setClass("DummyDBIConnectionSQLStrSub", representation("DBIConnection"))
 
-  dummy_con <- new("DummyDBIConnection")
-  sql_context <- dbplyr:::sql_context
-  prev_con <- sql_context$con
-  sql_context$con <- dummy_con
+  dummy_con <- new("DummyDBIConnectionSQLStrSub")
+  prev_con <- dbplyr:::set_current_con(dummy_con)
 
   expect_equal(sql_str_sub("SUBSTR")("string"), sql("SUBSTR('string', 1)"))
   expect_equal(sql_str_sub("SUBSTRING")("string"), sql("SUBSTRING('string', 1)"))
@@ -41,6 +38,6 @@ test_that("sql_str_sub works as expected", {
     sql("SUBSTRING('string', 4, LEN('string') - 4 + 1)")
     )
 
-  sql_context$con <- prev_con
+  dbplyr:::set_current_con(prev_con)
 })
 
