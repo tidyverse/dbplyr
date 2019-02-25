8b825d597e9eb69738f54bbf513e3d17466e0a2b
rlh1994 <r.l.hill128@gmail.com>
2019-02-25 10:06:22 +0000

    formatting

--- tests/testthat/test-backend-oracle.R
+++ tests/testthat/test-backend-oracle.R
@@ -1,30 +1,27 @@
 context("test-backend-oracle.R")
 
 test_that("custom scalar functions translated correctly", {
-
   trans <- function(x) {
     translate_sql(!!enquo(x), con = simulate_oracle())
   }
 
   expect_equal(trans(as.character(x)), sql("CAST(`x` AS VARCHAR2(255))"))
   expect_equal(trans(as.integer64(x)), sql("CAST(`x` AS NUMBER(19))"))
-  expect_equal(trans(as.double(x)),    sql("CAST(`x` AS NUMBER)"))
-  expect_equal(trans(as.Date(x)),    sql("DATE `x`"))
-  #expect_equal(trans(as.POSIXct(x)),    sql("DATE `x`"))
-  expect_equal(trans(as.Date(x, format = '%Y-%b-%d %X')),    sql("TO_DATE(`x`, 'YYYY-MON-DD HH24:MI:SS')"))
+  expect_equal(trans(as.double(x)), sql("CAST(`x` AS NUMBER)"))
+  expect_equal(trans(as.Date(x)), sql("DATE `x`"))
+  expect_equal(trans(as.Date(x, format = "%Y-%b-%d %X")), sql("TO_DATE(`x`, 'YYYY-MON-DD HH24:MI:SS')"))
   expect_equal(trans(today()), sql("TRUNC(SYSDATE)"))
   expect_equal(trans(now()), sql("SYSDATE"))
 
   expect_equal(trans(today() %>% `month<-`(3)), sql("add_months(to_date('01'||to_char(TRUNC(SYSDATE),'DDYYYYHH24MISS'), 'MMDDYYYYHH24MISS'),-1+'3')"))
-  expect_equal(trans(today() %>% `month<-`('Mar')), sql("add_months(to_date('01'||to_char(TRUNC(SYSDATE),'DDYYYYHH24MISS'), 'MMDDYYYYHH24MISS'),-1+'3')"))
-  expect_equal(trans(today() %>% `month<-`('October')), sql("add_months(to_date('01'||to_char(TRUNC(SYSDATE),'DDYYYYHH24MISS'), 'MMDDYYYYHH24MISS'),-1+'10')"))
-  expect_equal(trans(today() %>% `month<-`('17')), sql("add_months(to_date('01'||to_char(TRUNC(SYSDATE),'DDYYYYHH24MISS'), 'MMDDYYYYHH24MISS'),-1+'17')"))
+  expect_equal(trans(today() %>% `month<-`("Mar")), sql("add_months(to_date('01'||to_char(TRUNC(SYSDATE),'DDYYYYHH24MISS'), 'MMDDYYYYHH24MISS'),-1+'3')"))
+  expect_equal(trans(today() %>% `month<-`("October")), sql("add_months(to_date('01'||to_char(TRUNC(SYSDATE),'DDYYYYHH24MISS'), 'MMDDYYYYHH24MISS'),-1+'10')"))
+  expect_equal(trans(today() %>% `month<-`("17")), sql("add_months(to_date('01'||to_char(TRUNC(SYSDATE),'DDYYYYHH24MISS'), 'MMDDYYYYHH24MISS'),-1+'17')"))
 
   expect_equal(trans(today() %>% month(label = TRUE)), sql("EXTRACT(month FROM TRUNC(SYSDATE))"))
   expect_equal(trans(today() %>% hour()), sql("TO_CHAR(TRUNC(SYSDATE), 'HH24')"))
-  #expect_equal(trans(today() %>% am()), sql("'TRUE'"))
-  expect_equal(trans(as.Date('2020-02-05') %>% days_in_month()), sql("to_char(add_months(trunc(DATE '2020-02-05', 'mm'), 1)-1,'DD')"))
-  })
+  expect_equal(trans(as.Date("2020-02-05") %>% days_in_month()), sql("to_char(add_months(trunc(DATE '2020-02-05', 'mm'), 1)-1,'DD')"))
+})
 
 test_that("queries translate correctly", {
   mf <- lazy_frame(x = 1, src = simulate_oracle())
