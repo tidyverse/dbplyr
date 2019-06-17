44fadfbb382ed205d70513efaf6179ae186746c9
Hadley Wickham <h.wickham@gmail.com>
2019-06-17 10:04:08 -0500

    Add more informative warning for reactivevalues
    Fixes #322

--- R/escape.R
+++ R/escape.R
@@ -140,6 +140,20 @@ escape.data.frame <- function(x, parens = TRUE, collapse = ", ", con = NULL) {
 }
 
 #' @export
+escape.reactivevalues <- function(x, parens = TRUE, collapse = ", ", con = NULL) {
+  message <- paste0(
+    "Cannot embed a reactiveValues() object in a SQL query.\n\n",
+    "If you are seeing this error in code that used to work, the most likely ",
+    "cause is a change dbplyr 1.4.0. Previously `df$x` or `df[[y]]` implied ",
+    "that `df` was a local variable, but  now you must make that explict ",
+    " with `!!` or `local()`, e.g.,  `!!df$x` or `local(df[[\"y\"]))"
+  )
+
+  abort(paste(strwrap(message), collapse = "\n"))
+}
+
+
+#' @export
 #' @rdname escape
 sql_vector <- function(x, parens = NA, collapse = " ", con = NULL) {
   if (is.null(con)) {
