4fe564c95f4fea6b867895e3e78d7cede43cff70
Edgar Ruiz <edgar@rstudio.com>
2019-08-07 15:11:19 -0500

    Supports character tables, adds tests

--- R/tidyr-pivot.R
+++ R/tidyr-pivot.R
@@ -10,6 +10,9 @@ db_pivot_wider <- function(data,
                            values_fill = NULL,
                            values_fn = NULL,
                            spec = NULL) {
+  cn <- colnames(data)
+  names_from <- tidyselect::vars_select(cn, !!enquo(names_from))
+  values_from <- tidyselect::vars_select(cn, !!enquo(values_from))
 
   np <- "The `reparg`` argument is not supported for remote back-ends"
   assert_that(is.null(id_cols), msg = sub("reparg", "id_cols", np))
@@ -20,28 +23,30 @@ db_pivot_wider <- function(data,
   assert_that(is.null(spec), msg = sub("reparg", "spec", np))
 
 
-  cn <- colnames(data)
-  names_from <- tidyselect::vars_select(cn, !!enquo(names_from))
-  values_from <- tidyselect::vars_select(cn, !!enquo(values_from))
   pl <- c(values_from, names_from)
   kp <- cn[!(cn %in% pl)]
-  headers <- pull(summarise(group_by(data, !! sym(names_from))))
+  headers <- pull(summarise(group_by(data, !!sym(names_from))))
   mt <- purrr::map(
     headers,
-    ~{
+    ~ {
       header <- .x
       purrr::map(
         values_from,
-        ~expr(sum(ifelse(!! sym(names_from) == !! header, !! sym(.x), 0), na.rm = TRUE))
+        ~ expr(max(ifelse(!!sym(names_from) == !!header, !!sym(.x), NA), na.rm = TRUE))
       )
     }
   )
   fmt <- flatten(mt)
-  hn <- purrr::map(headers, ~paste0(values_from, "_", names_prefix, .x))
+  if (length(values_from) > 1) {
+    vp <- paste0(values_from, "_")
+  } else {
+    vp <- ""
+  }
+  hn <- purrr::map(headers, ~ paste0(vp, names_prefix, .x))
   rhn <- purrr::reduce(hn, c)
   nmt <- set_names(fmt, rhn)
-  grps <- group_by(data, !!! syms(kp))
-  summarise(grps, !!! nmt)
+  grps <- group_by(data, !!!syms(kp))
+  summarise(grps, !!!nmt)
 }
 
 globalVariables(c("name", "value"))
--- tests/testthat/test-tidyr-pivot.R
+++ tests/testthat/test-tidyr-pivot.R
@@ -2,15 +2,15 @@ context("pivot_wider")
 
 pw_frame <- memdb_frame(
   x = c("a", "a", "b", "b"),
-  y = c("x", "y", "x", "y"),
+  y = c("r", "t", "r", "t"),
   z = c(2, 2, 1, 1)
-  )
+)
 
 test_that("simple case returns expected result", {
   out <- pw_frame %>%
-    db_pivot_wider(names_from = y, values_from = z)  %>%
+    db_pivot_wider(names_from = y, values_from = z) %>%
     collect()
-  expect_equal(out[1, ], tibble(x = "a", z_x = 2, z_y = 2 ))
+  expect_equal(out[1, ], tibble(x = "a", r = 2, t = 2))
 })
 
 test_that("name_prefix is inserted to column names", {
@@ -19,9 +19,9 @@ test_that("name_prefix is inserted to column names", {
       names_from = y,
       values_from = z,
       names_prefix = "test_"
-      )  %>%
+    ) %>%
     collect()
-  expect_equal(out[1, ], tibble(x = "a", z_test_x = 2, z_test_y = 2 ))
+  expect_equal(out[1, ], tibble(x = "a", test_r = 2, test_t = 2))
 })
 
 test_that("simple case returns expected result", {
@@ -31,4 +31,32 @@ test_that("simple case returns expected result", {
   expect_error(db_pivot_wider(pw_frame, names_from = y, values_from = z, names_pattern = "test"))
   expect_error(db_pivot_wider(pw_frame, names_from = y, values_from = z, values_fill = "test"))
   expect_error(db_pivot_wider(pw_frame, names_from = y, values_from = z, values_fn = "test"))
+})
+
+test_that("all character table returns table as expected", {
+  out <- memdb_frame(
+    x = c("a", "a", "b", "b"),
+    y = c("x", "y", "x", "y"),
+    z = c("r", "t", "r", "t")
+  ) %>%
+    db_pivot_wider(
+      names_from = x,
+      values_from = z
+    ) %>%
+    collect()
+  expect_equal(out[1, ], tibble(y = "x", a = "r", b = "r"))
+})
+
+test_that("empty combinations return NA", {
+  out <- memdb_frame(
+    x = c("a", "a", "b", "d"),
+    y = c("x", "y", "x", "y"),
+    z = c("r", "t", "r", "t")
+  ) %>%
+    db_pivot_wider(
+      names_from = x,
+      values_from = z
+    ) %>%
+    collect()
+  expect_true(is.na(out[1, 4]))
 })
