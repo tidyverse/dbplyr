d8168fc546df9cae400ffbb10aec779b7ba63288
Edgar Ruiz <edgar@rstudio.com>
2019-08-07 16:04:44 -0500

    Moves unsupported args checks to a function

--- R/tidyr-pivot.R
+++ R/tidyr-pivot.R
@@ -10,19 +10,14 @@ db_pivot_wider <- function(data,
                            values_fill = NULL,
                            values_fn = NULL,
                            spec = NULL) {
+  check_null_pivot_args(
+    id_cols = !!id_cols, names_sep = !!names_sep,
+    names_repair = !!names_repair, values_fill = !!values_fill,
+    values_fn = !!values_fn, spec = !!spec
+  )
   cn <- colnames(data)
   names_from <- tidyselect::vars_select(cn, !!enquo(names_from))
   values_from <- tidyselect::vars_select(cn, !!enquo(values_from))
-
-  np <- "The `reparg`` argument is not supported for remote back-ends"
-  assert_that(is.null(id_cols), msg = sub("reparg", "id_cols", np))
-  assert_that(is.null(names_sep), msg = sub("reparg", "names_sep", np))
-  assert_that(is.null(names_repair), msg = sub("reparg", "names_repair", np))
-  assert_that(is.null(values_fill), msg = sub("reparg", "values_fill", np))
-  assert_that(is.null(values_fn), msg = sub("reparg", "values_fn", np))
-  assert_that(is.null(spec), msg = sub("reparg", "spec", np))
-
-
   pl <- c(values_from, names_from)
   kp <- cn[!(cn %in% pl)]
   headers <- pull(summarise(group_by(data, !!sym(names_from))))
@@ -47,6 +42,17 @@ db_pivot_wider <- function(data,
   nmt <- set_names(fmt, rhn)
   grps <- group_by(data, !!!syms(kp))
   summarise(grps, !!!nmt)
+}
+
+check_null_pivot_args <- function(..., msg = "The `{arg}` argument is not supported for remote back-ends") {
+  vars <- enquos(...)
+  purrr::imap(
+    vars,
+    ~ assert_that(
+      is.null(quo_get_expr(.x)),
+      msg = sub("\\{arg\\}", .y, msg)
+    )
+  )
 }
 
 globalVariables(c("name", "value"))
