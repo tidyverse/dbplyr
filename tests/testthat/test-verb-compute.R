63d094c4069c7c2f20d9e748131529f3fe158992
Hadley Wickham <h.wickham@gmail.com>
2020-04-14 14:22:19 -0500

    Fixes for newly sensitive tests

--- tests/testthat/test-verb-compute.R
+++ tests/testthat/test-verb-compute.R
@@ -67,13 +67,13 @@ test_that("compute creates correct column names", {
 
 # ops ---------------------------------------------------------------------
 
-test_that("preserved across compute and collapse", {
+test_that("sorting preserved across compute and collapse", {
   df1 <- memdb_frame(x = sample(10)) %>% arrange(x)
 
   df2 <- compute(df1)
-  expect_equal(op_sort(df2), list(~x))
+  expect_equal(get_expr(op_sort(df2)[[1]]), quote(x))
 
   df3 <- collapse(df1)
-  expect_equal(op_sort(df3), list(~x))
+  expect_equal(get_expr(op_sort(df3)[[1]]), quote(x))
 })
 
