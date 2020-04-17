0593a618b4ed1d3a21f45e58e6645f4f98890d89
Hadley Wickham <h.wickham@gmail.com>
2020-04-17 08:52:00 -0500

    Built site for dbplyr: 1.4.3@

--- pkgdown.js
+++ pkgdown.js
@@ -4,9 +4,9 @@
 
     $('.navbar-fixed-top').headroom();
 
-    $('body').scrollspy({
-      target: '#sidebar',
-      offset: 60
+    $('body').css('padding-top', $('.navbar').height() + 10);
+    $(window).resize(function(){
+      $('body').css('padding-top', $('.navbar').height() + 10);
     });
 
     $('[data-toggle="tooltip"]').tooltip();
