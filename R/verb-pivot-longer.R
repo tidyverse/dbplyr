15682bfd5f68c01e34fb184901e74fdfeb56601a
Maximilian Girlich <maximilian.girlich@metoda.com>
2020-11-16 10:44:28 +0100

    update documentation of `pivot_longer()`

--- R/verb-pivot-longer.R
+++ R/verb-pivot-longer.R
@@ -5,82 +5,63 @@
 #'
 #' `pivot_longer()` "lengthens" data, increasing the number of rows and
 #' decreasing the number of columns. The inverse transformation is
-#' `pivot_wider()`
+#' [pivot_wider.tbl_lazy()]
 #'
-#' Learn more in `vignette("pivot")`.
+#' Learn more in `vignette("pivot", "tidyr")`.
+#'
+#' While most functionality is identical there are some differences to
+#' `pivot_longer()` on local data frames:
+#' * the output is sorted differently/not explicitly,
+#' * the coercion of mixed column types is left to the database,
+#' * `values_ptypes` NOT supported.
+#'
+#' Note that `build_longer_spec()` and `pivot_longer_spec()` do not work with
+#' remote tables.
 #'
 #' @details
-#' `pivot_longer()` is an updated approach to [tidyr::gather()], designed to be both
-#' simpler to use and to handle more use cases. We recommend you use
-#' `pivot_longer()` for new code; `gather()` isn't going away but is no longer
-#' under active development.
+#' The SQL translation basically works as follows:
+#'
+#' 1. split the specification by its key columns i.e. by variables crammed
+#' into the column names.
+#' 2. for each part in the splitted specification `transmute()` `data` into the
+#' following columns
+#'   * id columns i.e. columns that are not pivotted
+#'   * key columns
+#'   * value columns i.e. columns that are pivotted
+#' 3. combine all the parts with `union_all()`
 #'
 #' @param data A data frame to pivot.
-#' @param cols Columns to pivot into
-#'   longer format.
+#' @param cols Columns to pivot into longer format.
 #' @param names_to A string specifying the name of the column to create
 #'   from the data stored in the column names of `data`.
-#'
-#'   Can be a character vector, creating multiple columns, if `names_sep`
-#'   or `names_pattern` is provided. In this case, there are two special
-#'   values you can take advantage of:
-#'
-#'   * `NA` will discard that component of the name.
-#'   * `.value` indicates that component of the name defines the name of the
-#'     column containing the cell values, overriding `values_to`.
 #' @param names_prefix A regular expression used to remove matching text
 #'   from the start of each variable name.
 #' @param names_sep,names_pattern If `names_to` contains multiple values,
 #'   these arguments control how the column name is broken up.
-#'
-#'   `names_sep` takes the same specification as [tidyr::separate()], and can either
-#'   be a numeric vector (specifying positions to break on), or a single string
-#'   (specifying a regular expression to split on).
-#'
-#'   `names_pattern` takes the same specification as [tidyr::extract()], a regular
-#'   expression containing matching groups (`()`).
-#'
-#'   If these arguments do not give you enough control, use
-#'   `pivot_longer_spec()` to create a spec object and process manually as
-#'   needed.
 #' @param names_repair What happens if the output has invalid column names?
-#'   The default, `"check_unique"` is to error if the columns are duplicated.
-#'   Use `"minimal"` to allow duplicates in the output, or `"unique"` to
-#'   de-duplicated by adding numeric suffixes. See [vctrs::vec_as_names()]
-#'   for more options.
 #' @param values_to A string specifying the name of the column to create
 #'   from the data stored in cell values. If `names_to` is a character
 #'   containing the special `.value` sentinel, this value will be ignored,
 #'   and the name of the value column will be derived from part of the
 #'   existing column names.
 #' @param values_drop_na If `TRUE`, will drop rows that contain only `NA`s
-#'   in the `value_to` column. This effectively converts explicit missing values
-#'   to implicit missing values, and should generally be used only when missing
-#'   values in `data` were created by its structure.
+#'   in the `value_to` column.
 #' @param names_transform,values_transform A list of column name-function pairs.
-#'   Use these arguments if you need to change the type of specific columns.
-#'   For example, `names_transform = list(week = as.integer)` would convert
-#'   a character week variable to an integer.
-#' @param names_ptypes,values_ptypes A list of column name-prototype pairs.
-#'   A prototype (or ptype for short) is a zero-length vector (like `integer()`
-#'   or `numeric()`) that defines the type, class, and attributes of a vector.
-#'   Use these arguments to confirm that the created columns are the types that
-#'   you expect.
-#'
-#'   If not specified, the type of the columns generated from `names_to` will
-#'   be character, and the type of the variables generated from `values_to`
-#'   will be the common type of the input columns used to generate them.
+#' @param names_ptypes A list of column name-prototype pairs.
+#' @param values_ptypes Not supported.
 #' @param ... Additional arguments passed on to methods.
-#' @export
-pivot_longer <- function(data, cols, names_to = "name", names_prefix = NULL,
-                         names_sep = NULL, names_pattern = NULL, names_ptypes = list(),
-                         names_transform = list(), names_repair = "check_unique",
-                         values_to = "value", values_drop_na = FALSE, values_ptypes = list(),
-                         values_transform = list(), ...) {
-  ellipsis::check_dots_used()
-  UseMethod("pivot_longer")
-}
-
+#' @examples
+#' # See vignette("pivot") for examples and explanation
+#'
+#' # Simplest case where column names are character data
+#' if (require("tidyr", quietly = TRUE)) {
+#'   memdb_frame(
+#'     id = c("a", "b"),
+#'     x = 1:2,
+#'     y = 3:4
+#'   ) %>%
+#'     pivot_longer(-id)
+#' }
 pivot_longer.tbl_lazy <- function(data,
                                   cols,
                                   names_to = "name",
@@ -98,6 +79,9 @@ pivot_longer.tbl_lazy <- function(data,
   if (!is_missing(values_ptypes)) {
     abort("The `values_ptypes` argument is not supported for remote back-ends")
   }
+
+  ellipsis::check_dots_empty()
+
   cols <- enquo(cols)
   spec <- dbplyr_build_longer_spec(data, !!cols,
     names_to = names_to,
@@ -203,16 +187,6 @@ dbplyr_pivot_longer_spec <- function(data,
 
   id_cols <- syms(setdiff(colnames(data), spec$.name))
 
-  # the values of `.name` (first column) go to the column described in `.value`
-  # (second column). The "keys", i.e. the other columns - if any - describe the
-  # value further.
-  # Each name in `.value` stands for one column in the long format.
-  # Two rows with the same values in the "keys" imply that the should end up
-  # in the same row in the long format.
-
-  # 1. split spec according to "keys"
-  # 2. iterate over splitted spec and pivot_long for each part
-  # 3. row bind the results
   spec_split <- vctrs::vec_split(spec, spec[, -(1:2)])
   data_long_list <- purrr::map(
     vctrs::vec_seq_along(spec_split),
