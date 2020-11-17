041378d1c1982985da8a0b4b8d6c42bdf35a259e
Maximilian Girlich <maximilian.girlich@metoda.com>
2020-11-17 08:09:58 +0100

    remove unnecessary code

--- R/verb-pivot-wider.R
+++ R/verb-pivot-wider.R
@@ -31,17 +31,6 @@ pivot_wider.tbl_lazy <- function(data,
   )
 }
 
-check_null_pivot_args <- function(..., msg = "The `{arg}` argument is not supported for remote back-ends") {
-  vars <- enquos(...)
-  purrr::imap(
-    vars,
-    ~ assert_that(
-      is.null(quo_get_expr(.x)),
-      msg = sub("\\{arg\\}", .y, msg)
-    )
-  )
-}
-
 dbplyr_build_wider_spec <- function(data,
                                     names_from = name,
                                     values_from = value,
@@ -159,45 +148,6 @@ globalVariables(c("name", "value"))
 
 
 # move to other file ------------------------------------------------------
-
-# Not a great name as it now also casts
-vals_dedup <- function(key, val, value, summarize = NULL) {
-
-  if (is.null(summarize)) {
-    if (!vec_duplicate_any(key)) {
-      return(list(key = key, val = val))
-    }
-
-    warn(glue::glue(
-      "Values are not uniquely identified; output will contain list-cols.\n",
-      "* Use `values_fn = list` to suppress this warning.\n",
-      "* Use `values_fn = length` to identify where the duplicates arise\n",
-      "* Use `values_fn = {{summary_fun}}` to summarise duplicates"
-    ))
-  }
-
-  out <- vec_split(val, key)
-  if (!is.null(summarize) && !identical(summarize, list)) {
-    summarize <- as_function(summarize)
-    # This is only correct if `values_fn` always returns a single value
-    # Needs https://github.com/r-lib/vctrs/issues/183
-    out$val <- vec_c(!!!map(out$val, summarize))
-  }
-
-  out
-}
-
-# Wrap a "rectangular" vector into a data frame
-wrap_vec <- function(vec, names) {
-  ncol <- length(names)
-  nrow <- vec_size(vec) / ncol
-  out <- set_names(vec_init(list(), ncol), names)
-  for (i in 1:ncol) {
-    out[[i]] <- vec_slice(vec, ((i - 1) * nrow + 1):(i * nrow))
-  }
-
-  as_tibble(out)
-}
 
 is_scalar <- function(x) {
   if (is.null(x)) {
