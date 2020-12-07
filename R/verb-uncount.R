bc178033b4be30eaaf897f4bb3472defc6b4057a
Maximilian Girlich <maximilian.girlich@metoda.com>
2020-12-07 11:05:31 +0100

    add verb `uncount()`

---
+++ R/verb-uncount.R
@@ -0,0 +1,60 @@
+#' "Uncount" a database table
+#'
+#' This is a method for the tidyr `uncount()` generic.
+#'
+#' @inheritParams arrange.tbl_lazy
+#' @param weights A vector of weights. Evaluated in the context of `data`;
+#'   supports quasiquotation.
+#' @param .id Supply a string to create a new variable which gives a unique
+#'   identifier for each created row.
+#' @param .remove If `TRUE`, and `weights` is the name of a column in `data`,
+#'   then this column is removed.
+#' @export
+#' @examples
+#' df <- memdb_frame(x = c("a", "b"), n = c(1, 2))
+#' dbplyr_uncount(df, n)
+#' dbplyr_uncount(df, n, .id = "id")
+#'
+#' # You can also use constants
+#' dbplyr_uncount(df, 2)
+#'
+#' # Or expressions
+#' dbplyr_uncount(df, 2 / n)
+dbplyr_uncount <- function(data, weights, .remove = TRUE, .id = NULL) {
+  weights_quo <- enquo(weights)
+  weights_col <- "..dbplyr_weight_col"
+  data <- mutate(
+    data,
+    !!weights_col := !!weights_quo,
+  )
+
+  n_max <- summarise(ungroup(data), max(!!sym(weights_col), na.rm = TRUE)) %>% pull()
+  n_max <- vctrs::vec_cast(n_max, integer(), x_arg = "weights")
+
+  if (is_null(.id)) {
+    row_id_col <- "..dbplyr_row_id"
+  } else {
+    row_id_col <- .id
+  }
+  sql_on_expr <- expr(RHS[[!!row_id_col]] <= LHS[[!!weights_col]])
+
+  data_uncounted <- inner_join(
+    data,
+    tibble(!!row_id_col := seq2(1, n_max)),
+    by = character(),
+    sql_on = translate_sql(!!sql_on_expr, con = remote_con(data)),
+    copy = TRUE
+  )
+
+  if (is_null(.id)) {
+    data_uncounted <- select(data_uncounted, -!!row_id_col, -!!weights_col)
+  } else {
+    data_uncounted <- select(data_uncounted, -!!weights_col)
+  }
+
+  if (is_true(.remove) && quo_is_symbol(weights_quo)) {
+    data_uncounted <- select(data_uncounted, -quo_name(weights_quo))
+  }
+
+  data_uncounted
+}
---
+++ tests/testthat/test-verb-uncount.R
@@ -0,0 +1,49 @@
+test_that("symbols weights are dropped in output", {
+  df <- memdb_frame(x = 1, w = 1)
+  expect_equal(dbplyr_uncount(df, w) %>% collect(), tibble(x = 1))
+})
+
+test_that("can request to preserve symbols", {
+  df <- memdb_frame(x = 1, w = 1)
+  expect_equal(dbplyr_uncount(df, w, .remove = FALSE) %>% collect(), df %>% collect())
+})
+
+test_that("unique identifiers created on request", {
+  df <- memdb_frame(w = 1:3)
+  expect_equal(
+    dbplyr_uncount(df, w, .id = "id") %>% collect(),
+    tibble(id = c(1L, 1:2, 1:3))
+  )
+})
+
+test_that("expands constants and expressions", {
+  df <- memdb_frame(x = 1, w = 2)
+
+  expect_equal(dbplyr_uncount(df, 2) %>% collect(), collect(df)[c(1, 1), ])
+  expect_equal(dbplyr_uncount(df, 1 + 1) %>% collect(), collect(df)[c(1, 1), ])
+})
+
+
+test_that("works with groups", {
+  df <- memdb_frame(g = 1, x = 1, w = 1) %>% dplyr::group_by(g)
+  expect_equal(group_vars(dbplyr_uncount(df, w)), "g")
+})
+
+test_that("doesn't remove grouping variable", {
+  df <- memdb_frame(g = 1, x = 1, w = 1) %>% dplyr::group_by(g)
+
+  expect_equal(dbplyr_uncount(df, g) %>% collect(), df %>% collect())
+})
+
+test_that("must evaluate to integer", {
+  df <- memdb_frame(x = 1, w = 1/2)
+  expect_error(dbplyr_uncount(df, w), class = "vctrs_error_cast_lossy")
+
+  df <- memdb_frame(x = 1)
+  expect_error(dbplyr_uncount(df, "W"), class = "vctrs_error_incompatible_type")
+})
+
+test_that("works with 0 weights", {
+  df <- memdb_frame(x = 1:2, w = c(0, 1))
+  expect_equal(dbplyr_uncount(df, w) %>% collect(), tibble(x = 2))
+})
