b9dd8d662dd64d0d7d5df80bf8d97c395fcdc873
Maximilian Girlich <maximilian.girlich@metoda.com>
2021-01-18 09:59:32 +0000

    replace `local_package()` with `library()` and `detach()`

--- tests/testthat/test-verb-expand.R
+++ tests/testthat/test-verb-expand.R
@@ -1,6 +1,5 @@
 test_that("expand completes all values", {
-  skip_if_not_installed("tidyr")
-  withr::local_package("tidyr")
+  suppressPackageStartupMessages(library(tidyr))
 
   expect_equal(
     memdb_frame(x = 1:2, y = 1:2) %>% expand(x, y) %>% collect(),
@@ -8,11 +7,12 @@ test_that("expand completes all values", {
   )
 
   expect_snapshot(lazy_frame(x = 1, y = 1) %>% expand(x, y))
+
+  detach("package:tidyr")
 })
 
 test_that("nesting doesn't expand values", {
-  skip_if_not_installed("tidyr")
-  withr::local_package("tidyr")
+  suppressPackageStartupMessages(library(tidyr))
 
   df <- tibble(x = 1:2, y = 1:2)
   expect_equal(
@@ -23,20 +23,22 @@ test_that("nesting doesn't expand values", {
 
   df_lazy <- lazy_frame(!!!df)
   expect_snapshot(df_lazy %>% expand(nesting(x, y)))
+
+  detach("package:tidyr")
 })
 
 test_that("expand accepts expressions", {
-  skip_if_not_installed("tidyr")
-  withr::local_package("tidyr")
+  suppressPackageStartupMessages(library(tidyr))
 
   df <- lazy_frame(x = 1)
   expect_snapshot(expand(df, round(x / 2)))
   expect_snapshot(expand(df, nesting(x_half = round(x / 2), x1 = x + 1)))
+
+  detach("package:tidyr")
 })
 
 test_that("expand respects groups", {
-  skip_if_not_installed("tidyr")
-  withr::local_package("tidyr")
+  suppressPackageStartupMessages(library(tidyr))
 
   df <- tibble(
     a = c(1L, 1L, 2L),
@@ -55,40 +57,45 @@ test_that("expand respects groups", {
 
   df_lazy <- lazy_frame(!!!df)
   expect_snapshot(df_lazy %>% group_by(a) %>% expand(b, c))
+
+  detach("package:tidyr")
 })
 
 test_that("NULL inputs", {
-  skip_if_not_installed("tidyr")
-  withr::local_package("tidyr")
+  suppressPackageStartupMessages(library(tidyr))
 
   expect_snapshot(expand(lazy_frame(x = 1), x, y = NULL))
+
+  detach("package:tidyr")
 })
 
 test_that("expand() errors when expected", {
-  skip_if_not_installed("tidyr")
-  withr::local_package("tidyr")
+  suppressPackageStartupMessages(library(tidyr))
 
   expect_snapshot_error(expand(memdb_frame(x = 1)))
   expect_snapshot_error(expand(memdb_frame(x = 1), x = NULL))
+
+  detach("package:tidyr")
 })
 
 test_that("expand respect .name_repair", {
-  skip_if_not_installed("tidyr")
-  withr::local_package("tidyr")
+  suppressPackageStartupMessages(library(tidyr))
 
-  expect_named(
+  vars <- suppressMessages(
     memdb_frame(x = integer(), z = integer()) %>%
       expand(x, z = x, nesting(x), nesting(z), .name_repair = "unique") %>%
-      collect(),
-    c("x...1", "z...2", "x...3", "z...4")
+      op_vars()
   )
+
+  expect_equal(vars, c("x...1", "z...2", "x...3", "z...4"))
+
+  detach("package:tidyr")
 })
 
 # replace_na --------------------------------------------------------------
 
 test_that("replace_na replaces missing values", {
-  skip_if_not_installed("tidyr")
-  withr::local_package("tidyr")
+  suppressPackageStartupMessages(library(tidyr))
 
   expect_equal(
     memdb_frame(x = c(1, NA), y = c(NA, "b")) %>%
@@ -103,20 +110,22 @@ test_that("replace_na replaces missing values", {
   expect_snapshot(
     lazy_frame(x = 1, y = "a") %>% replace_na(list(x = 0, y = "unknown"))
   )
+
+  detach("package:tidyr")
 })
 
 test_that("replace_na ignores missing columns", {
-  skip_if_not_installed("tidyr")
-  withr::local_package("tidyr")
+  suppressPackageStartupMessages(library(tidyr))
 
   expect_snapshot(lazy_frame(x = 1) %>% replace_na(list(not_there = 0)))
+
+  detach("package:tidyr")
 })
 
 # complete ----------------------------------------------------------------
 
 test_that("complete completes missing combinations", {
-  skip_if_not_installed("tidyr")
-  withr::local_package("tidyr")
+  suppressPackageStartupMessages(library(tidyr))
 
   df <- tibble(
     x = 1:2,
@@ -135,4 +144,6 @@ test_that("complete completes missing combinations", {
 
   df_lazy <- lazy_frame(!!!df)
   expect_snapshot(df_lazy %>% complete(x, y, fill = list(z = "c")))
+
+  detach("package:tidyr")
 })
