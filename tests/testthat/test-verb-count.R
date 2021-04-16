bc9848deb439ba00d927aaad80c56dba7f6e4fa6
Maximilian Girlich <maximilian.girlich@metoda.com>
2021-04-16 05:57:04 +0000

    tests for `add_count.tbl_lazy()`

--- tests/testthat/test-verb-count.R
+++ tests/testthat/test-verb-count.R
@@ -4,6 +4,8 @@ test_that("generates expected SQL for common situations", {
   expect_snapshot(db %>% count(g))
   expect_snapshot(db %>% count(g, wt = x))
   expect_snapshot(db %>% count(g, sort = TRUE))
+
+  expect_snapshot(db %>% add_count(g, sort = TRUE))
 })
 
 test_that("preserves group of input", {
@@ -11,6 +13,8 @@ test_that("preserves group of input", {
   expect_equal(db %>% count(g) %>% group_vars(), character())
   expect_equal(db %>% group_by(g) %>% count() %>% group_vars(), "g")
   expect_equal(db %>% group_by(g, x) %>% count() %>% group_vars(), c("g", "x"))
+
+  expect_equal(db %>% group_by(g, x) %>% add_count(g) %>% group_vars(), c("g", "x"))
 })
 
 test_that("complains about bad names", {
