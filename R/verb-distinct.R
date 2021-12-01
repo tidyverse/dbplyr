3088c944170c39674a90eb79a34bbdc38c744391
Maximilian Girlich <maximilian.girlich@metoda.com>
2021-12-01 14:54:19 +0000

    `distinct()` sets `select` field

--- R/verb-distinct.R
+++ R/verb-distinct.R
@@ -36,6 +36,7 @@ add_distinct <- function(.data) {
   lazy_select_query(
     from = lazy_query,
     last_op = "distinct",
+    select = syms(set_names(op_vars(lazy_query))),
     distinct = TRUE
   )
 }
