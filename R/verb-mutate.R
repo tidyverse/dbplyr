d7371b7b2027c070d5f63e31acf9148ac82ebb2e
Maximilian Girlich <maximilian.girlich@metoda.com>
2021-12-02 14:55:29 +0000

    `add_mutate()` now optimises if previous op was `select()`

--- R/verb-mutate.R
+++ R/verb-mutate.R
@@ -72,6 +72,41 @@ nest_vars <- function(.data, dots, all_vars) {
 add_mutate <- function(.data, vars) {
   lazy_query <- .data$lazy_query
 
+  # TODO avoid code duplication with `add_select()`
+  if (identical(lazy_query$last_op, "select")) {
+    # Special optimisation when applied to pure projection() - this is
+    # conservative and we could expand to any op_select() if combined with
+    # the logic in nest_vars()
+    select <- lazy_query$select
+
+    if (purrr::every(vars, is.symbol)) {
+      # if current operation is pure projection
+      # we can just subset the previous selection
+      sel_vars <- purrr::map_chr(vars, as_string)
+      lazy_query <- update_lazy_select(lazy_query, sel_vars)
+
+      return(lazy_query)
+    }
+
+    prev_vars <- select$expr
+    if (purrr::every(prev_vars, is.symbol)) {
+      # if previous operation is pure projection
+      sel_vars <- purrr::map_chr(prev_vars, as_string)
+      if (all(select$name == sel_vars)) {
+        # and there's no renaming
+        # we can just ignore the previous step
+        lazy_query$select <- new_lazy_select(
+          vars,
+          group_vars = op_grps(lazy_query),
+          order_vars = op_sort(lazy_query),
+          frame = op_frame(lazy_query)
+        )
+        # lazy_query <- update_lazy_select(lazy_query, vars)
+        return(lazy_query)
+      }
+    }
+  }
+
   lazy_select_query(
     from = lazy_query,
     last_op = "mutate",
