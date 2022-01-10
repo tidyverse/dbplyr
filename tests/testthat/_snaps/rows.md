29b69b5f69a69901862e0fde58d483577b55107a
Maximilian Girlich <maximilian.girlich@metoda.com>
2022-01-10 10:53:59 +0000

    Add tests

---
+++ tests/testthat/_snaps/rows.md
@@ -0,0 +1,47 @@
+# early return if no column to update
+
+    Code
+      rows_update(lazy_frame(x = 1:3, y = 11:13, .name = "df_x"), lazy_frame(x = 1:3,
+      .name = "df_y"), by = "x", in_place = FALSE)
+    Output
+      <SQL>
+      SELECT *
+      FROM `df_x`
+
+# empty `by` works
+
+    Code
+      rows_update(lazy_frame(x = 1:3, y = 11:13, .name = "df_x"), lazy_frame(x = 1:3,
+      .name = "df_y"), in_place = FALSE)
+    Message <dplyr_message_matching_by>
+      Matching, by = "x"
+    Output
+      <SQL>
+      SELECT *
+      FROM `df_x`
+
+# `in_place = FALSE` works
+
+    Code
+      rows_update(lazy_frame(x = 1:3, y = 11:13, .name = "df_x"), lazy_frame(x = 2:3,
+      y = 22:23, .name = "df_y"), by = "x", in_place = FALSE)
+    Output
+      <SQL>
+      (
+        SELECT * FROM `df_x` AS `LHS`
+        WHERE NOT EXISTS (
+          SELECT 1 FROM `df_y` AS `RHS`
+          WHERE (`LHS`.`x` = `RHS`.`x`)
+        )
+      )
+      UNION ALL
+      (
+        SELECT `LHS`.`x` AS `x`, `y`
+        FROM (
+          SELECT `x`
+          FROM `df_x`
+        ) `LHS`
+        INNER JOIN `df_y` AS `RHS`
+          ON (`LHS`.`x` = `RHS`.`x`)
+      )
+
---
+++ tests/testthat/test-rows.R
@@ -0,0 +1,120 @@
+
+# rows_update -------------------------------------------------------------
+
+test_that("early return if no column to update", {
+  expect_snapshot(
+    rows_update(
+      lazy_frame(x = 1:3, y = 11:13, .name = "df_x"),
+      lazy_frame(x = 1:3, .name = "df_y"),
+      by = "x",
+      in_place = FALSE
+    )
+  )
+
+  expect_equal(
+    rows_update(
+      memdb_frame(x = 1:3, y = 11:13),
+      memdb_frame(x = 1:3),
+      by = "x",
+      in_place = FALSE
+    ) %>%
+      collect(),
+    tibble(x = 1:3, y = 11:13)
+  )
+
+  expect_equal(
+    rows_update(
+      memdb_frame(x = 1:3, y = 11:13),
+      memdb_frame(x = 1:3),
+      by = "x",
+      in_place = TRUE
+    ) %>%
+      collect(),
+    tibble(x = 1:3, y = 11:13)
+  )
+})
+
+test_that("empty `by` works", {
+  expect_snapshot(
+    rows_update(
+      lazy_frame(x = 1:3, y = 11:13, .name = "df_x"),
+      lazy_frame(x = 1:3, .name = "df_y"),
+      in_place = FALSE
+    )
+  )
+})
+
+test_that("`in_place = FALSE` works", {
+  expect_snapshot(
+    rows_update(
+      lazy_frame(x = 1:3, y = 11:13, .name = "df_x"),
+      lazy_frame(x = 2:3, y = 22:23, .name = "df_y"),
+      by = "x",
+      in_place = FALSE
+    )
+  )
+
+  df <- memdb_frame(x = 1:3, y = 11:13)
+  expect_equal(
+    rows_update(
+      df, memdb_frame(x = 2:3, y = 22:23),
+      by = "x",
+      in_place = FALSE
+    ) %>%
+      collect(),
+    tibble(x = 1:3, y = c(11L, 22:23))
+  )
+
+  expect_equal(df %>% collect(), tibble(x = 1:3, y = 11:13))
+})
+
+test_that("`in_place = FALSE` with `returning` works", {
+  expect_equal(
+    rows_update(
+      memdb_frame(x = 1:3, y = 11:13),
+      memdb_frame(x = 2:3, y = 22:23),
+      by = "x",
+      in_place = FALSE,
+      returning = quote(everything())
+    ) %>%
+      get_returned_rows(),
+    tibble(x = 2:3, y = 22:23)
+  )
+})
+
+test_that("`in_place = TRUE` works", {
+  # TODO error for `in_place = TRUE` and lazy frames
+  # expect_snapshot(
+  #   rows_update(
+  #     lazy_frame(x = 1:3, y = 11:13, .name = "df_x"),
+  #     lazy_frame(x = 2:3, y = 22:23, .name = "df_y"),
+  #     by = "x",
+  #     in_place = TRUE
+  #   )
+  # )
+
+  df <- memdb_frame(x = 1:3, y = 11:13)
+  rows_update(
+    df, memdb_frame(x = 2:3, y = 22:23),
+    by = "x",
+    in_place = TRUE
+  )
+
+  expect_equal(df %>% collect(), tibble(x = 1:3, y = c(11L, 22:23)))
+})
+
+test_that("`in_place = TRUE` with `returning` works", {
+  skip_if_not_installed("RSQLite", "2.2.8")
+
+  df <- memdb_frame(x = 1:3, y = 11:13)
+  df_updated <- rows_update(
+    df, memdb_frame(x = 2:4, y = 22:24),
+    by = "x",
+    in_place = TRUE,
+    returning = quote(everything())
+  )
+
+  expect_equal(get_returned_rows(df_updated), tibble(x = 2:3, y = 22:23))
+
+  expect_equal(df_updated %>% collect(), tibble(x = 1:3, y = c(11L, 22:23)))
+})
