3ff4a1401920feb2d4da1617edbf580e8281d572
Maximilian Girlich <maximilian.girlich@metoda.com>
2022-01-11 13:19:14 +0000

    Test `rows_delete()`

--- tests/testthat/_snaps/rows.md
+++ tests/testthat/_snaps/rows.md
@@ -42,6 +42,21 @@
       SELECT *
       FROM `df_x`
 
+---
+
+    Code
+      rows_delete(lazy_frame(x = 1:3, y = c(11, 12, NA), .name = "df_x"), lazy_frame(
+        x = 1:3, .name = "df_y"), in_place = FALSE)
+    Message <dplyr_message_matching_by>
+      Matching, by = "x"
+    Output
+      <SQL>
+      SELECT * FROM `df_x` AS `LHS`
+      WHERE NOT EXISTS (
+        SELECT 1 FROM `df_y` AS `RHS`
+        WHERE (`LHS`.`x` = `RHS`.`x`)
+      )
+
 # `in_place = FALSE` works
 
     Code
@@ -90,5 +105,18 @@
           INNER JOIN `df_y` AS `RHS`
             ON (`LHS`.`x` = `RHS`.`x`)
         ) `q01`
+      )
+
+---
+
+    Code
+      rows_delete(lazy_frame(x = 1:3, y = c(11, 12, NA), .name = "df_x"), lazy_frame(
+        x = 2:3, y = 22:23, .name = "df_y"), by = "x", in_place = FALSE)
+    Output
+      <SQL>
+      SELECT * FROM `df_x` AS `LHS`
+      WHERE NOT EXISTS (
+        SELECT 1 FROM `df_y` AS `RHS`
+        WHERE (`LHS`.`x` = `RHS`.`x`)
       )
 
--- tests/testthat/test-rows.R
+++ tests/testthat/test-rows.R
@@ -239,3 +239,92 @@ test_that("`in_place = TRUE` with `returning` works", {
 
   expect_equal(df_patched %>% collect(), tibble(x = 1:3, y = c(11, 12, 23)))
 })
+
+
+
+# rows_delete -------------------------------------------------------------
+
+test_that("empty `by` works", {
+  expect_snapshot(
+    rows_delete(
+      lazy_frame(x = 1:3, y = c(11, 12, NA), .name = "df_x"),
+      lazy_frame(x = 1:3, .name = "df_y"),
+      in_place = FALSE
+    )
+  )
+})
+
+test_that("`in_place = FALSE` works", {
+  expect_snapshot(
+    rows_delete(
+      lazy_frame(x = 1:3, y = c(11, 12, NA), .name = "df_x"),
+      lazy_frame(x = 2:3, y = 22:23, .name = "df_y"),
+      by = "x",
+      in_place = FALSE
+    )
+  )
+
+  df <- memdb_frame(x = 1:3, y = c(11, 12, NA))
+  expect_equal(
+    rows_delete(
+      df, memdb_frame(x = 2:3, y = 22:23),
+      by = "x",
+      in_place = FALSE
+    ) %>%
+      collect(),
+    tibble(x = 1, y = 11)
+  )
+
+  expect_equal(df %>% collect(), tibble(x = 1:3, y = c(11, 12, NA)))
+})
+
+test_that("`in_place = FALSE` with `returning` works", {
+  expect_equal(
+    rows_delete(
+      memdb_frame(x = 1:3, y = c(11, 12, 13)),
+      memdb_frame(x = 2:3, y = 22:23),
+      by = "x",
+      in_place = FALSE,
+      returning = quote(everything())
+    ) %>%
+      get_returned_rows(),
+    tibble(x = 2:3, y = 12:13)
+  )
+})
+
+test_that("`in_place = TRUE` works", {
+  # TODO error for `in_place = TRUE` and lazy frames
+  # expect_snapshot(
+  #   rows_update(
+  #     lazy_frame(x = 1:3, y = 11:13, .name = "df_x"),
+  #     lazy_frame(x = 2:3, y = 22:23, .name = "df_y"),
+  #     by = "x",
+  #     in_place = TRUE
+  #   )
+  # )
+
+  df <- memdb_frame(x = 1:3, y = 11:13)
+  rows_delete(
+    df, memdb_frame(x = 2:3, y = 22:23),
+    by = "x",
+    in_place = TRUE
+  )
+
+  expect_equal(df %>% collect(), tibble(x = 1L, y = 11L))
+})
+
+test_that("`in_place = TRUE` with `returning` works", {
+  skip_if_not_installed("RSQLite", "2.2.8")
+
+  df <- memdb_frame(x = 1:3, y = 11:13)
+  df_deleted <- rows_delete(
+    df, memdb_frame(x = 2:4, y = 22:24),
+    by = "x",
+    in_place = TRUE,
+    returning = quote(everything())
+  )
+
+  expect_equal(get_returned_rows(df_deleted), tibble(x = 2:3, y = 12:13))
+
+  expect_equal(df_deleted %>% collect(), tibble(x = 1L, y = 11L))
+})
