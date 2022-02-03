48bab79fb9721ba1c2cedbe3ba199802ce4ddac5
Maximilian Girlich <maximilian.girlich@metoda.com>
2022-02-03 12:11:52 +0000

    Add more tests

--- tests/testthat/test-verb-count.R
+++ tests/testthat/test-verb-count.R
@@ -6,6 +6,7 @@ test_that("generates expected SQL for common situations", {
   expect_snapshot(db %>% count(g, sort = TRUE))
 
   expect_snapshot(db %>% add_count(g, sort = TRUE))
+  expect_snapshot(db %>% group_by(g) %>% add_count())
 })
 
 test_that("preserves group of input", {
@@ -15,11 +16,19 @@ test_that("preserves group of input", {
   expect_equal(db %>% group_by(g, x) %>% count() %>% group_vars(), c("g", "x"))
 
   expect_equal(db %>% group_by(g, x) %>% add_count(g) %>% group_vars(), c("g", "x"))
+  expect_equal(db %>% add_count(g) %>% group_vars(), character())
 })
 
 test_that("complains about bad names", {
   expect_snapshot(error = TRUE, {
     db <- lazy_frame(g = 1, x = 2)
     db %>% count(g, name = "g")
+  })
+})
+
+test_that(".drop is not supported", {
+  expect_snapshot(error = TRUE, {
+    lazy_frame(g = 1) %>%
+      add_count(.drop = TRUE)
   })
 })
