100f7f719fed6686fee73fddd3e528f4a6fbcbb7
Maximilian Girlich <maximilian.girlich@metoda.com>
2022-04-11 07:55:59 +0000

    Replace snapshot tests by `expect_equal()`

--- tests/testthat/test-translate-sql-window.R
+++ tests/testthat/test-translate-sql-window.R
@@ -173,53 +173,109 @@ test_that("names windows automatically", {
     group_by(part) %>%
     window_order(ord)
 
-  expect_snapshot({
-    # window only used once -> no need to name the window
-    lf %>%
-      mutate(
-        across(c(col1), sum, na.rm = TRUE),
-        across(c(col3), ~ order_by(desc(ord), cumsum(.x)))
-      )
+  lf1 <- lf %>%
+    transmute(
+      across(c(col1, col2), sum, na.rm = TRUE),
+      across(c(col3, col4), ~ order_by(desc(ord), cumsum(.x)))
+    )
 
-    # uses 2 different window
-    lf %>%
-      mutate(
-        across(c(col1, col2), sum, na.rm = TRUE),
-        across(c(col3, col4), ~ order_by(desc(ord), cumsum(.x)))
-      )
-  })
+  sql_list <- get_select_sql(lf1$lazy_query$select, "mutate", op_vars(lf), simulate_sqlite())
+  expect_equal(
+    sql_list$window_sql,
+    sql(
+      "`win1` AS (PARTITION BY `part`)",
+      "`win2` AS (PARTITION BY `part` ORDER BY `ord` DESC ROWS UNBOUNDED PRECEDING)"
+    )
+  )
+  expect_equal(
+    sql_list$select_sql,
+    sql(
+      part = ident("part"),
+      col1 = sql("SUM(`col1`) OVER `win1`"),
+      col2 = sql("SUM(`col2`) OVER `win1`"),
+      col3 = sql("SUM(`col3`) OVER `win2`"),
+      col4 = sql("SUM(`col4`) OVER `win2`")
+    )
+  )
 
   # Different order does not confuse naming of windows
-  expect_snapshot(
-    lf %>%
-      transmute(
-        col1 = sum(col1, na.rm = TRUE),
-        col3 = order_by(desc(ord), cumsum(col3)),
-        col2 = sum(col2, na.rm = TRUE),
-        col4 = order_by(desc(ord), cumsum(col4))
-      )
+  lf2 <- lf %>%
+    transmute(
+      col1 = sum(col1, na.rm = TRUE),
+      col3 = order_by(desc(ord), cumsum(col3)),
+      col2 = sum(col2, na.rm = TRUE),
+      col4 = order_by(desc(ord), cumsum(col4))
+    )
+
+  sql_list <- get_select_sql(lf2$lazy_query$select, "mutate", op_vars(lf), simulate_sqlite())
+  expect_equal(
+    sql_list$window_sql,
+    sql(
+      "`win1` AS (PARTITION BY `part`)",
+      "`win2` AS (PARTITION BY `part` ORDER BY `ord` DESC ROWS UNBOUNDED PRECEDING)"
+    )
+  )
+  expect_equal(
+    sql_list$select_sql,
+    sql(
+      part = ident("part"),
+      col1 = sql("SUM(`col1`) OVER `win1`"),
+      col3 = sql("SUM(`col3`) OVER `win2`"),
+      col2 = sql("SUM(`col2`) OVER `win1`"),
+      col4 = sql("SUM(`col4`) OVER `win2`")
+    )
+  )
+})
+
+test_that("only name windows if they appear multiple times", {
+  lf <- lazy_frame(
+    col1 = runif(3),
+    col2 = runif(3),
+    col3 = runif(3),
+    part = c("a", "a", "b"),
+    ord = 3:1,
+    con = simulate_sqlite()
+  ) %>%
+    group_by(part) %>%
+    window_order(ord) %>%
+    transmute(
+      across(c(col1, col2), sum, na.rm = TRUE),
+      across(c(col3), ~ order_by(desc(ord), cumsum(.x)))
+    )
+
+  sql_list <- get_select_sql(lf$lazy_query$select, "mutate", op_vars(lf), simulate_sqlite())
+  expect_equal(sql_list$window_sql, sql("`win1` AS (PARTITION BY `part`)"))
+  expect_equal(
+    sql_list$select_sql,
+    sql(
+      part = ident("part"),
+      col1 = sql("SUM(`col1`) OVER `win1`"),
+      col2 = sql("SUM(`col2`) OVER `win1`"),
+      col3 = sql("SUM(`col3`) OVER (PARTITION BY `part` ORDER BY `ord` DESC ROWS UNBOUNDED PRECEDING)")
+    )
   )
 })
 
 test_that("name windows only if supported", {
-  lf2 <- lazy_frame(
+  lf <- lazy_frame(
     col1 = runif(3),
     col2 = runif(3),
-    col3 = runif(3),
-    col4 = runif(3),
     part = c("a", "a", "b"),
-    ord = 3:1,
     con = simulate_hana()
   ) %>%
     group_by(part) %>%
-    window_order(ord)
+    transmute(
+      across(c(col1, col2), sum, na.rm = TRUE)
+    )
 
-  # Does not use named windows as they are not supported
-  expect_snapshot({
-    lf2 %>%
-      mutate(
-        across(c(col1, col2), sum, na.rm = TRUE),
-        across(c(col3, col4), ~ order_by(desc(ord), cumsum(.x)))
-      )
-  })
+  sql_list <- get_select_sql(lf$lazy_query$select, "mutate", op_vars(lf), simulate_hana())
+  expect_equal(sql_list$window_sql, character())
+  expect_equal(
+    sql_list$select_sql,
+    sql(
+      part = ident("part"),
+      col1 = sql("SUM(`col1`) OVER (PARTITION BY `part`)"),
+      col2 = sql("SUM(`col2`) OVER (PARTITION BY `part`)")
+    )
+  )
 })
