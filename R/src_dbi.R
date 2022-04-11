75bb29b03304c3feec1d6ba1b74879a5c3d3b7d6
Kirill Müller <krlmlr+r@mailbox.org>
2022-04-11 07:17:45 +0200

    Avoid override

--- R/src_dbi.R
+++ R/src_dbi.R
@@ -88,10 +88,6 @@ tbl.src_dbi <- function(src, from, ...) {
 # Internal calls to `tbl()` should be avoided in favor of tbl_src_dbi().
 # The former may query the database for column names if `vars` is omitted,
 # the latter always requires `vars`.
-tbl <- function(...) {
-  abort("Use `tbl_src_dbi()` instead")
-}
-
 tbl_src_dbi <- function(src, from, vars) {
   subclass <- class(src$con)[[1]] # prefix added by dplyr::make_tbl
   tbl_sql_impl(c(subclass, "dbi"), src, from, vars)
