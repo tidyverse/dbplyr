796d432a8a75077e80bc60d017924f555c95f276
Kirill Müller <krlmlr+r@mailbox.org>
2022-04-30 09:01:19 +0200

    New in_catalog()

--- R/schema.R
+++ R/schema.R
@@ -1,10 +1,16 @@
-#' Refer to a table in a schema
+#' Refer to a table in a schema or a database catalog
 #'
-#' @param schema,table Names of schema and table. These will be automatically
-#'   quoted; use `sql()` to pass a raw name that won't get quoted.
+#' `in_schema()` can be used in [tbl()] to indicate a table in a specific
+#' schema.
+#' `in_catalog()` additionally allows specifying the database catalog.
+#'
+#' @param catalog,schema,table Names of catalog, schema, and table.
+#'   These will be automatically quoted; use [sql()] to pass a raw name
+#'   that won't get quoted.
 #' @export
 #' @examples
 #' in_schema("my_schema", "my_table")
+#' in_catalog("my_catalog", "my_schema", "my_table")
 #' # eliminate quotes
 #' in_schema(sql("my_schema"), sql("my_table"))
 #'
@@ -22,20 +28,42 @@
 #' con %>% tbl("df")
 #' con %>% tbl(in_schema("aux", "df"))
 in_schema <- function(schema, table) {
+  in_catalog(NULL, schema, table)
+}
+
+#' @rdname in_schema
+#' @export
+in_catalog <- function(catalog, schema, table) {
   structure(
-    list(schema = as.sql(schema), table = as.sql(table)),
+    list(
+      schema = as.sql(schema),
+      table = as.sql(table),
+      catalog = if (!is.null(catalog)) as.sql(catalog)
+    ),
     class = "dbplyr_schema"
   )
 }
 
 #' @export
 print.dbplyr_schema <- function(x, ...) {
-  cat_line("<SCHEMA> ", escape_ansi(x$schema), ".", escape_ansi(x$table))
+  if (is.null(x$catalog)) {
+    cat_line("<SCHEMA> ", escape_ansi(x$schema), ".", escape_ansi(x$table))
+  } else {
+    cat_line("<CATALOG> ", escape_ansi(x$catalog), ".", escape_ansi(x$schema), ".", escape_ansi(x$table))
+  }
 }
 
 #' @export
 as.sql.dbplyr_schema <- function(x, con) {
-  ident_q(paste0(escape(x$schema, con = con), ".", escape(x$table, con = con)))
+  if (is.null(x$catalog)) {
+    ident_q(paste0(
+      escape(x$schema, con = con), ".", escape(x$table, con = con)
+    ))
+  } else {
+    ident_q(paste0(
+      escape(x$catalog, con = con), ".", escape(x$schema, con = con), ".", escape(x$table, con = con)
+    ))
+  }
 }
 
 is.schema <- function(x) inherits(x, "dbplyr_schema")
