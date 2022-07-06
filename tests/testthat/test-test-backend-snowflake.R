c01888def50bd60953649f57cbef11a48797b640
Anthony Frachioni <anthony.frachioni@flatiron.com>
2022-07-05 22:08:11 -0400

    first test for more snowflake translations

--- tests/testthat/test-test-backend-snowflake.R
+++ tests/testthat/test-test-backend-snowflake.R
@@ -1,4 +1,5 @@
 test_that("custom scalar translated correctly", {
   local_con(simulate_snowflake())
   expect_equal(translate_sql(log10(x)), sql("LOG(10.0, `x`)"))
+  expect_equal(translate_sql(round(x, digits = 1.1)), sql("ROUND((`x`) :: FLOAT, 1)"))
 })
