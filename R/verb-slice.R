77df1ac6012a3edc2dc42ddf01e21fa461a741cf
Maximilian Girlich <maximilian.girlich@metoda.com>
2022-12-07 14:49:06 +0000

    Use `check_required()` in `slice_min/max()`

--- R/verb-slice.R
+++ R/verb-slice.R
@@ -75,9 +75,7 @@ slice_tail.tbl_lazy <- function(.data, ..., n, prop, by = NULL) {
 #' @importFrom dplyr slice_min
 #' @export
 slice_min.tbl_lazy <- function(.data, order_by, ..., n, prop, by = NULL, with_ties = TRUE) {
-  if (missing(order_by)) {
-    cli_abort("Argument {.arg order_by} is missing, with no default.")
-  }
+  check_required(order_by)
   size <- check_slice_size(n, prop)
   slice_by(.data, {{order_by}}, size, {{ by }}, with_ties = with_ties)
 }
@@ -86,9 +84,7 @@ slice_min.tbl_lazy <- function(.data, order_by, ..., n, prop, by = NULL, with_ti
 #' @importFrom dplyr slice_max
 #' @export
 slice_max.tbl_lazy <- function(.data, order_by, ..., n, by = NULL, prop, with_ties = TRUE) {
-  if (missing(order_by)) {
-    cli_abort("Argument {.arg order_by} is missing, with no default.")
-  }
+  check_required(order_by)
   size <- check_slice_size(n, prop)
 
   slice_by(.data, desc({{order_by}}), size, {{ by }}, with_ties = with_ties)
