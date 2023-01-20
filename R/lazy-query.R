c19f1fffebc249d9f6287a8cddef8620315039ac
Maximilian Girlich <maximilian.girlich@metoda.com>
2023-01-20 07:59:33 +0000

    Check `group_vars` in `lazy_query()`

--- R/lazy-query.R
+++ R/lazy-query.R
@@ -6,7 +6,7 @@ lazy_query <- function(query_type,
                        group_vars = op_grps(x),
                        order_vars = op_sort(x),
                        frame = op_frame(x)) {
-  # stopifnot(is.null(group_vars) || (is.character(group_vars) && is.null(names(group_vars))))
+  stopifnot(is.null(group_vars) || is.character(group_vars), is.null(names(group_vars)))
   stopifnot(is.null(order_vars) || is_expression_list(order_vars), is.null(names(order_vars)))
   stopifnot(is.null(frame) || is_integerish(frame$range, n = 2))
 
