35264d370ada32f30c2efd3730eccace27c2f5c2
Maximilian Girlich <maximilian.girlich@metoda.com>
2023-01-30 13:51:34 +0000

    Import `join_by()`

--- R/dbplyr.R
+++ R/dbplyr.R
@@ -4,7 +4,7 @@
 #' @importFrom utils head tail
 #' @importFrom glue glue
 #' @importFrom cli cli_abort
-#' @importFrom dplyr n
+#' @importFrom dplyr n join_by
 #' @import rlang
 #' @import DBI
 #' @importFrom tibble tibble as_tibble
