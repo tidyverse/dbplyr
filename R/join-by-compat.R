d48a0c6ee829b5f31f1fa13bfafebbd208ca976e
Maximilian Girlich <maximilian.girlich@metoda.com>
2023-01-30 13:51:53 +0000

    Update `join-by-compat.R`

--- R/join-by-compat.R
+++ R/join-by-compat.R
@@ -1,9 +1,12 @@
-new_join_by <- function(exprs, condition, filter, cross, x, y) {
+new_join_by <- function(exprs = list(),
+                        condition = character(),
+                        filter = character(),
+                        x = character(),
+                        y = character()) {
   out <- list(
     exprs = exprs,
     condition = condition,
     filter = filter,
-    cross = cross,
     x = x,
     y = y
   )
@@ -48,22 +51,35 @@ as_join_by.list <- function(x, error_call = caller_env()) {
   # TODO: check lengths
   x_names <- x[["x"]]
   y_names <- x[["y"]]
+
+  if (!is_character(x_names)) {
+    abort("`by$x` must evaluate to a character vector.")
+  }
+  if (!is_character(y_names)) {
+    abort("`by$y` must evaluate to a character vector.")
+  }
+
   finalise_equi_join_by(x_names, y_names)
 }
 
 finalise_equi_join_by <- function(x_names, y_names) {
   n <- length(x_names)
 
+  if (n == 0L) {
+    abort(
+      "Backwards compatible support for cross joins should have been caught earlier.",
+      .internal = TRUE
+    )
+  }
+
   exprs <- purrr::map2(x_names, y_names, function(x, y) expr(!!x == !!y))
   condition <- vctrs::vec_rep("==", times = n)
   filter <- vctrs::vec_rep("none", times = n)
-  cross <- n == 0L
 
   new_join_by(
     exprs = exprs,
     condition = condition,
     filter = filter,
-    cross = cross,
     x = x_names,
     y = y_names
   )
@@ -82,12 +98,11 @@ join_by_common <- function(x_names,
   if (length(by) == 0) {
     message <- c(
       "`by` must be supplied when `x` and `y` have no common variables.",
-      i = "Use `by = join_by()` to perform a cross-join."
+      i = "Use `cross_join()` to perform a cross-join."
     )
     abort(message, call = error_call)
   }
 
-  # by_names <- tick_if_needed(by)
   by_names <- by
   by_names <- glue::glue_collapse(by_names, sep = ", ")
   inform(glue("Joining with `by = join_by({by_names})`"))
