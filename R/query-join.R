b63e3f93d5a9d5b78d60d54076ed989b6c9542bb
Maximilian Girlich <maximilian.girlich@metoda.com>
2023-03-14 15:33:55 +0000

    Fix complaint about non-empty dots

--- R/query-join.R
+++ R/query-join.R
@@ -77,7 +77,7 @@ sql_render.multi_join_query <- function(query, con = NULL, ..., subquery = FALSE
   x <- sql_render(query$x, con, ..., subquery = TRUE, lvl = lvl + 1)
   query$joins$table <- purrr::map(
     query$joins$table,
-    ~ sql_render(.x, con, ..., subquery = TRUE, lvl = lvl + 1)
+    function(table) sql_render(table, con, ..., subquery = TRUE, lvl = lvl + 1)
   )
 
   sql_query_multi_join(
