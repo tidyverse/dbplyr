47c276ee22e5c38db23dc59e9cf7f2eeb4ba854a
Maximilian Girlich <maximilian.girlich@metoda.com>
2023-04-18 09:21:48 +0000

    Support namespaced functions in `across()`

--- NEWS.md
+++ NEWS.md
@@ -1,5 +1,8 @@
 # dbplyr (development version)
 
+* Using a function with a namespace in `across()` now works, e.g.
+  `across(x, dplyr::dense_rank)` (@mgirlich, #1231).
+
 * Can now use `select()` again after using `arrange(desc(x))` (@ejneer, #1240).
 
 * The first argument of `ntile()` has been renamed from `order_by` to `x` to
--- R/tidyeval-across.R
+++ R/tidyeval-across.R
@@ -86,6 +86,8 @@ across_funs <- function(funs, env, data, dots, names_spec, fn, evaluated = FALSE
     names_spec <- names_spec %||% "{.col}"
   } else if (is_quosure(funs)) {
     return(across_funs(quo_squash(funs), env, data, dots, names_spec, fn, evaluated = evaluated))
+  } else if (is_call(funs, "::")) {
+    return(across_funs(funs[[3]], env, data, dots, names_spec, fn, evaluated = evaluated))
   } else if (is_symbol(funs) || is_function(funs) ||
              is_call(funs, "~") || is_call(funs, "function")) {
     is_local_list <- function(funs) {
