33cbde7bb2802438cc46a2c3a1fcd8d7a06150c7
Eric Neer <ericjneer@gmail.com>
2024-03-04 10:49:49 -0800

    re-add snapshot showing arranged computed column isn't inlined away

--- tests/testthat/test-verb-select.R
+++ tests/testthat/test-verb-select.R
@@ -309,6 +309,33 @@ test_that("where() isn't suppored", {
   })
 })
 
+test_that("arranged computed columns are not inlined away", {
+  lf <- lazy_frame(x = 1)
+
+  # shouldn't inline
+  out <- lf %>% mutate(z = 2) %>% arrange(x, z) %>% select(x)
+  # should inline
+  out2 <- lf %>% mutate(z = 2) %>% arrange(x, z) %>% select(x, z)
+
+  inner_query <- out$lazy_query$x
+  expect_snapshot({
+      lf %>% mutate(z = 1) %>% arrange(x, z) %>% select(x)
+  })
+  expect_s3_class(inner_query, "lazy_select_query")
+  expect_equal(
+    inner_query$order_by,
+    list(quo(x), quo(z)),
+    ignore_formula_env = TRUE
+  )
+  expect_equal(op_vars(inner_query), c("x", "z"))
+  expect_equal(op_vars(out$lazy_query), "x")
+  expect_equal(
+      # order vars in a subquery are dropped
+      inner_query[setdiff(names(inner_query), "order_vars")],
+      out2$lazy_query[setdiff(names(out2$lazy_query), "order_vars")]
+  )
+})
+
 # sql_render --------------------------------------------------------------
 
 test_that("multiple selects are collapsed", {
