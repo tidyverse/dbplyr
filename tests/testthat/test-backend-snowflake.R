b6c7314053a1a0ff7fed605cb5ca8cd38249c90e
simonpcouch <simonpatrickcouch@gmail.com>
2024-10-21 07:18:30 -0700

    transition `expect_error()`
    * ...to snapshot when testing the error message
    * ...to classed error when testing an error from another package or an error thrown from a widely-used helper
    * ...to `expect_no_error()` (or no test) when `regex = NA`
    * Analogous story for `expect_warning()` and `expect_message()`.

--- tests/testthat/test-backend-snowflake.R
+++ tests/testthat/test-backend-snowflake.R
@@ -14,7 +14,7 @@ test_that("pasting translated correctly", {
   expect_equal(test_translate_sql(str_c(x, y)), sql("CONCAT_WS('', `x`, `y`)"))
   expect_equal(test_translate_sql(str_c(x, y, sep = "|")), sql("CONCAT_WS('|', `x`, `y`)"))
 
-  expect_error(test_translate_sql(paste0(x, collapse = "")), "`collapse` not supported")
+  expect_snapshot(error = TRUE, test_translate_sql(paste0(x, collapse = "")))
 
   expect_equal(test_translate_sql(str_flatten(x), window = TRUE), sql("LISTAGG(`x`, '') OVER ()"))
   expect_equal(test_translate_sql(str_flatten(x, collapse = "|"), window = TRUE), sql("LISTAGG(`x`, '|') OVER ()"))
@@ -87,7 +87,10 @@ test_that("custom lubridate functions translated correctly", {
   ))
   expect_equal(test_translate_sql(quarter(x)), sql("EXTRACT('quarter', `x`)"))
   expect_equal(test_translate_sql(quarter(x, with_year = TRUE)), sql("(EXTRACT('year', `x`) || '.' || EXTRACT('quarter', `x`))"))
-  expect_error(test_translate_sql(quarter(x, fiscal_start = 2)))
+  expect_error(
+    test_translate_sql(quarter(x, fiscal_start = 2)),
+    class = "unsupported_arg_error"
+  )
   expect_equal(test_translate_sql(isoyear(x)), sql("EXTRACT('year', `x`)"))
 
   expect_equal(test_translate_sql(seconds(x)), sql("INTERVAL '`x` second'"))
@@ -106,7 +109,10 @@ test_that("custom clock functions translated correctly", {
   local_con(simulate_snowflake())
   expect_equal(test_translate_sql(add_years(x, 1)), sql("DATEADD(YEAR, 1.0, `x`)"))
   expect_equal(test_translate_sql(add_days(x, 1)), sql("DATEADD(DAY, 1.0, `x`)"))
-  expect_error(test_translate_sql(add_days(x, 1, "dots", "must", "be empty")))
+  expect_error(
+    test_translate_sql(add_days(x, 1, "dots", "must", "be empty")),
+    class = "rlib_error_dots_nonempty"
+  )
   expect_equal(test_translate_sql(date_build(2020, 1, 1)), sql("DATE_FROM_PARTS(2020.0, 1.0, 1.0)"))
   expect_equal(test_translate_sql(date_build(year_column, 1L, 1L)), sql("DATE_FROM_PARTS(`year_column`, 1, 1)"))
   expect_equal(test_translate_sql(get_year(date_column)), sql("DATE_PART(YEAR, `date_column`)"))
@@ -119,8 +125,14 @@ test_that("difftime is translated correctly", {
   expect_equal(test_translate_sql(difftime(start_date, end_date, units = "days")), sql("DATEDIFF(DAY, `end_date`, `start_date`)"))
   expect_equal(test_translate_sql(difftime(start_date, end_date)), sql("DATEDIFF(DAY, `end_date`, `start_date`)"))
 
-  expect_error(test_translate_sql(difftime(start_date, end_date, units = "auto")))
-  expect_error(test_translate_sql(difftime(start_date, end_date, tz = "UTC", units = "days")))
+  expect_snapshot(
+    error = TRUE,
+    test_translate_sql(difftime(start_date, end_date, units = "auto"))
+  )
+  expect_snapshot(
+    error = TRUE,
+    test_translate_sql(difftime(start_date, end_date, tz = "UTC", units = "days"))
+  )
 })
 
 test_that("min() and max()", {
