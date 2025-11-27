8503cee8acf8dda77cf944ce078f75f3116e0613
Hadley Wickham <h.wickham@gmail.com>
2025-11-27 09:16:05 +0200

    Don't create new function; just document

--- R/explain.R
+++ R/explain.R
@@ -1,8 +1,24 @@
+#' Show generated SQL and query plan
+#'
+#' `show_sql()` displays the SQL query that will be dispatched to the database;
+#' `explain()` displays both the SQL query and the query plan.
+#'
+#' @param ... For `explain()`, further arguments to [remote_query_plan()].
+#'   For `show_query()`, ignored.
+#' @param x An object to explain
 #' @importFrom dplyr show_query
 #' @export
-#' @rdname show_sql
-show_query.tbl_lazy <- function(x, ..., cte = FALSE, sql_options = NULL) {
-  withr::local_options(list(dbplyr_use_colour = TRUE))
+#' @param use_colour Should the output be coloured?
+#' @inheritParams remote_query
+#' @name show_query
+show_query.tbl_lazy <- function(
+  x,
+  ...,
+  use_colour = TRUE,
+  cte = FALSE,
+  sql_options = NULL
+) {
+  withr::local_options(list(dbplyr_use_colour = use_colour))
   sql <- remote_query(x, cte = cte, sql_options = sql_options)
   cat_line("<SQL>")
   cat_line(sql)
@@ -11,8 +27,7 @@ show_query.tbl_lazy <- function(x, ..., cte = FALSE, sql_options = NULL) {
 
 #' @importFrom dplyr explain
 #' @export
-#' @describeIn show_sql Combine the `show_query()` and `remote_query_plan()`
-#'   summaries of `x`.
+#' @rdname show_query
 explain.tbl_sql <- function(x, ...) {
   force(x)
   show_query(x)
@@ -22,22 +37,3 @@ explain.tbl_sql <- function(x, ...) {
 
   invisible(x)
 }
-
-#' Show the query backing a tbl
-#'
-#' These functions all break the abstraction layer and directly display
-#'   the underlying query that will be dispatched to the SQL engine
-#'   backing a connection.
-#'
-#' @inheritParams remote_query
-#' @param x An object to explain
-#' @param ... For `explain()`, further arguments to [remote_query_plan()].
-#'   For `show_sql()`, further arguments to other methods. For `show_query()`,
-#'   ignored.
-#' @export
-show_sql <- function(x, ...) {
-  UseMethod("show_sql")
-}
-
-#' @export
-show_sql.tbl_lazy <- show_query.tbl_lazy
--- tests/testthat/test-explain.R
+++ tests/testthat/test-explain.R
@@ -1,19 +1,8 @@
-test_that("show_sql() prints query and returns self", {
+test_that("show_query() prints query", {
   lf <- lazy_frame(x = 1)
   out <- lf |>
     select(x) |>
     filter(x > 0)
 
-  expect_snapshot(show_sql(out))
-  expect_identical(show_sql(out), out)
-  expect_invisible(show_sql(out))
-})
-
-test_that("show_sql() can produce CTEs", {
-  lf <- lazy_frame(x = 1, y = 2)
-  out <- lf |>
-    mutate(z = x + y) |>
-    left_join(lf, by = c("x", "y"))
-
-  expect_snapshot(show_sql(out, cte = TRUE))
+  expect_snapshot(show_query(out))
 })
