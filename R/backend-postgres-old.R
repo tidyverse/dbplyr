9eaeed96f8818324e2dbf2e1f7dbae5a8c7f4dc7
Hadley Wickham <h.wickham@gmail.com>
2025-12-02 13:09:46 -0600

    Restore minimal old Postgres

--- R/backend-postgres-old.R
+++ R/backend-postgres-old.R
@@ -5,7 +5,7 @@ NULL
 # Use dbplyr edition 1 for custom method dispatch on RPostgreSQL connections
 #' @export
 dbplyr_edition.PostgreSQLConnection <- function(con) {
-  1L
+  2L
 }
 
 # #' @export
@@ -34,7 +34,6 @@ dbplyr_edition.PostgreSQLConnection <- function(con) {
 #     ...,
 #     row.names = FALSE
 #   )
-
 #   table
 # }
 
@@ -49,26 +48,26 @@ dbplyr_edition.PostgreSQLConnection <- function(con) {
 #   dbGetInfo(qry)$fieldDescription[[1]]$name
 # }
 
-# #' @export
-# db_connection_describe.PostgreSQLConnection <- db_connection_describe.PqConnection
+#' @export
+db_connection_describe.PostgreSQLConnection <- db_connection_describe.PqConnection
 
-# #' @export
-# sql_translation.PostgreSQLConnection <- sql_translation.PqConnection
+#' @export
+sql_translation.PostgreSQLConnection <- sql_translation.PqConnection
 
-# #' @export
-# sql_expr_matches.PostgreSQLConnection <- sql_expr_matches.PqConnection
+#' @export
+sql_expr_matches.PostgreSQLConnection <- sql_expr_matches.PqConnection
 
-# #' @export
-# sql_query_explain.PostgreSQLConnection <- sql_query_explain.PqConnection
+#' @export
+sql_query_explain.PostgreSQLConnection <- sql_query_explain.PqConnection
 
-# #' @export
-# supports_window_clause.PostgreSQLConnection <- function(con) {
-#   TRUE
-# }
+#' @export
+supports_window_clause.PostgreSQLConnection <- function(con) {
+  TRUE
+}
 
-# #' @export
-# sql_query_insert.PostgreSQLConnection <- sql_query_insert.PostgreSQL
+#' @export
+sql_query_insert.PostgreSQLConnection <- sql_query_insert.PostgreSQL
 
-# #' @export
-# sql_query_upsert.PostgreSQLConnection <- sql_query_upsert.PostgreSQL
+#' @export
+sql_query_upsert.PostgreSQLConnection <- sql_query_upsert.PostgreSQL
 # nocov end
