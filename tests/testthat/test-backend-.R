58f51fc198454b0a5d2c6c952150200b4b4de3f8
Hadley Wickham <h.wickham@gmail.com>
2025-12-18 11:14:43 -0600

    Move DDL tests to db-sql.R

--- tests/testthat/test-backend-.R
+++ tests/testthat/test-backend-.R
@@ -198,30 +198,3 @@ test_that("default raw escapes translated correctly", {
   qry <- mf |> filter(x %in% !!L)
   expect_snapshot(qry)
 })
-
-# DDL ---------------------------------------------------------------------
-
-test_that("DDL operations generate expected SQL", {
-  con <- simulate_dbi()
-
-  expect_snapshot(sql_table_analyze(con, in_schema("schema", "tbl")))
-  expect_snapshot(sql_query_explain(con, sql("SELECT * FROM foo")))
-
-  expect_snapshot(sql_query_wrap(con, ident("table")))
-  expect_snapshot(sql_query_wrap(con, in_schema("schema", "tbl")))
-  expect_snapshot(sql_query_wrap(con, sql("SELECT * FROM foo")))
-
-  expect_snapshot(sql_table_index(con, in_schema("schema", "tbl"), c("a", "b")))
-  expect_snapshot(sql_table_index(
-    con,
-    in_schema("schema", "tbl"),
-    "c",
-    unique = TRUE
-  ))
-
-  expect_snapshot(sql_query_save(
-    con,
-    sql("SELECT * FROM foo"),
-    in_schema("temp", "tbl")
-  ))
-})
--- tests/testthat/test-db-sql.R
+++ tests/testthat/test-db-sql.R
@@ -14,3 +14,30 @@ test_that("sql_query_rows() works", {
     sql('SELECT COUNT(*) FROM "abc" AS "master"')
   )
 })
+
+test_that("analyse/explain sql generates expected SQL", {
+  con <- simulate_dbi()
+  expect_snapshot(sql_table_analyze(con, "tbl"))
+  expect_snapshot(sql_query_explain(con, sql("SELECT * FROM foo")))
+})
+
+test_that("sql_query_wrap generates expected SQL", {
+  con <- simulate_dbi()
+  expect_snapshot(sql_query_wrap(con, ident("table")))
+  expect_snapshot(sql_query_wrap(con, in_schema("schema", "tbl")))
+  expect_snapshot(sql_query_wrap(con, sql("SELECT * FROM foo")))
+})
+
+test_that("sql_table_index generates expected SQL", {
+  con <- simulate_dbi()
+  expect_snapshot(sql_table_index(con, "tbl", c("a", "b")))
+  expect_snapshot(sql_table_index(con, "tbl", "c", unique = TRUE))
+})
+
+test_that("sql_query_save generates expected SQL", {
+  con <- simulate_dbi()
+
+  sql <- sql("SELECT * FROM foo")
+  expect_snapshot(sql_query_save(con, sql, "tbl"))
+  expect_snapshot(sql_query_save(con, sql, "tbl", temporary = FALSE))
+})
