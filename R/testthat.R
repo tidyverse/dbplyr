75af706f774799b21c123a3892437738ce1f7126
Hadley Wickham <h.wickham@gmail.com>
2025-12-18 11:39:50 -0600

    Delete `expect_equal_tbls()`

--- R/testthat.R
+++ R/testthat.R
@@ -7,37 +7,3 @@ compare_tbl <- function(x, y, label = NULL, expected.label = NULL) {
   )
 }
 
-expect_equal_tbls <- function(results, ref = NULL, ...) {
-  check_list(results)
-
-  if (!is_named(results)) {
-    result_name <- expr_name(substitute(results)) # nocov
-    names(results) <- paste0(result_name, "_", seq_along(results)) # nocov
-  }
-
-  # If ref is NULL, use the first result
-  if (is.null(ref)) {
-    if (length(results) < 2) {
-      testthat::skip("Need at least two srcs to compare")
-    }
-
-    ref <- results[[1]]
-    ref_name <- names(results)[[1]]
-
-    rest <- results[-1]
-  } else {
-    rest <- results
-    ref_name <- "`ref`"
-  }
-
-  for (i in seq_along(rest)) {
-    compare_tbl(
-      rest[[i]],
-      ref,
-      label = names(rest)[[i]],
-      expected.label = ref_name
-    )
-  }
-
-  invisible(TRUE)
-}
