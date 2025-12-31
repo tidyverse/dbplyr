e0e067e82c9da36e7687686c21581085477f4860
Hadley Wickham <h.wickham@gmail.com>
2025-12-31 11:15:52 -0600

    Move polishing

--- tests/testthat/test-backend-spark-sql.R
+++ tests/testthat/test-backend-spark-sql.R
@@ -8,115 +8,56 @@ test_that("str_like and str_ilike work", {
 
 test_that("custom clock functions translated correctly", {
   con <- simulate_spark_sql()
-  expect_translation(
-    con,
-    add_years(x, 1),
-    'ADD_MONTHS("x", 1.0 * 12)'
-  )
+  expect_translation(con, add_years(x, 1), 'ADD_MONTHS("x", 1.0 * 12)')
   expect_translation(con, add_days(x, 1), 'DATE_ADD("x", 1.0)')
   expect_error(
     translate_sql(add_days(x, 1, "dots", "must", "be empty"), con = con),
     class = "rlib_error_dots_nonempty"
   )
-  expect_translation(
-    con,
-    date_build(2020, 1, 1),
-    "MAKE_DATE(2020.0, 1.0, 1.0)"
-  )
-  expect_translation(
-    con,
-    date_build(year_column, 1L, 1L),
-    'MAKE_DATE("year_column", 1, 1)'
-  )
-  expect_translation(
-    con,
-    get_year(date_column),
-    'DATE_PART(\'YEAR\', "date_column")'
-  )
-  expect_translation(
-    con,
-    get_month(date_column),
-    'DATE_PART(\'MONTH\', "date_column")'
-  )
-  expect_translation(
-    con,
-    get_day(date_column),
-    'DATE_PART(\'DAY\', "date_column")'
-  )
-  expect_translation(
-    con,
-    date_count_between(date_column_1, date_column_2, "day"),
-    'DATEDIFF("date_column_2", "date_column_1")'
-  )
-  expect_snapshot(
-    error = TRUE,
-    translate_sql(
-      date_count_between(date_column_1, date_column_2, "year"),
-      con = con
-    )
-  )
-  expect_snapshot(
-    error = TRUE,
-    translate_sql(
-      date_count_between(
-        date_column_1,
-        date_column_2,
-        "day",
-        n = 5
-      ),
-      con = con
-    )
-  )
+  expect_translation(con, date_build(2020L, 1L, 1L), "MAKE_DATE(2020, 1, 1)")
+  expect_translation(con, date_build(x, 1L, 1L), 'MAKE_DATE("x", 1, 1)')
+  expect_translation(con, get_year(x), 'DATE_PART(\'YEAR\', "x")')
+  expect_translation(con, get_month(x), 'DATE_PART(\'MONTH\', "x")')
+  expect_translation(con, get_day(x), 'DATE_PART(\'DAY\', "x")')
+  expect_translation(con, date_count_between(x, y, "day"), 'DATEDIFF("y", "x")')
+  expect_translation_error(con, date_count_between(x, y, "year"))
+  expect_translation_error(con, date_count_between(x, y, "day", n = 5))
 })
 
 test_that("difftime is translated correctly", {
   con <- simulate_spark_sql()
-  expect_translation(
-    con,
-    difftime(start_date, end_date, units = "days"),
-    'DATEDIFF("end_date", "start_date")'
-  )
-  expect_translation(con, difftime(start, end), 'DATEDIFF("end", "start")')
-
-  expect_snapshot(
-    error = TRUE,
-    translate_sql(difftime(start_date, end_date, units = "auto"), con = con)
-  )
-  expect_snapshot(
-    error = TRUE,
-    translate_sql(
-      difftime(
-        start_date,
-        end_date,
-        tz = "UTC",
-        units = "days"
-      ),
-      con = con
-    )
-  )
+  expect_translation(con, difftime(x, y, units = "days"), 'DATEDIFF("y", "x")')
+  expect_translation(con, difftime(x, y), 'DATEDIFF("y", "x")')
+  expect_translation_error(con, difftime(x, y, units = "auto"))
+  expect_translation_error(con, difftime(x, y, tz = "UTC"))
 })
 
 test_that("first and last aggregate functions work", {
   con <- simulate_spark_sql()
   expect_translation(con, first(x), 'FIRST("x")', window = FALSE)
   expect_translation(con, last(x), 'LAST("x")', window = FALSE)
+  expect_translation_error(con, nth(x, 2), 'LAST("x")', window = FALSE)
 })
 
 test_that("first, last, and nth window functions work", {
   con <- simulate_spark_sql()
-  expect_equal(
-    translate_sql(first(x, na_rm = TRUE), vars_order = "a", con = con),
-    sql('FIRST_VALUE("x", TRUE) OVER (ORDER BY "a")')
+  expect_translation(
+    con,
+    first(x, na_rm = TRUE),
+    'FIRST_VALUE("x", TRUE) OVER (ORDER BY "a")',
+    vars_order = "a"
   )
-  expect_equal(
-    translate_sql(last(x, na_rm = TRUE), vars_order = "a", con = con),
-    sql(
-      'LAST_VALUE("x", TRUE) OVER (ORDER BY "a" ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING)'
-    )
+  expect_translation(
+    con,
+    last(x, na_rm = TRUE),
+    'LAST_VALUE("x", TRUE) OVER (ORDER BY "a" ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING)',
+    vars_order = "a"
   )
-  expect_equal(
-    translate_sql(nth(x, 2, na_rm = TRUE), vars_order = "a", con = con),
-    sql('NTH_VALUE("x", 2, TRUE) OVER (ORDER BY "a")')
+  expect_translation(
+    con,
+    nth(x, 2, na_rm = TRUE),
+    'NTH_VALUE("x", 2, TRUE) OVER (ORDER BY "a")',
+    vars_order = "a"
   )
 })
 
@@ -141,15 +82,15 @@ test_that("db_copy_to and db_compute generate correct SQL", {
     .package = "DBI"
   )
 
-  db_compute(con, "test_table", sql("SELECT 1"), temporary = TRUE)
+  db_compute(con, "t", sql("SELECT 1"), temporary = TRUE)
   expect_match(executed_sql, "CREATE TEMPORARY VIEW")
 
-  db_compute(con, "test_table", sql("SELECT 1"), temporary = FALSE)
+  db_compute(con, "t", sql("SELECT 1"), temporary = FALSE)
   expect_match(executed_sql, "CREATE TABLE")
 
-  db_compute(con, "test_table", sql("SELECT 1"), overwrite = TRUE)
+  db_compute(con, "t", sql("SELECT 1"), overwrite = TRUE)
   expect_match(executed_sql, "CREATE OR REPLACE")
 
-  db_copy_to(con, "test_table", data.frame(x = 1))
+  db_copy_to(con, "t", data.frame(x = 1))
   expect_match(executed_sql, "CREATE TEMPORARY VIEW")
 })
